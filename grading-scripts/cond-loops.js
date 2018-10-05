170fdfa77d49f3c376fbbf723cb0a14f6f58831a
mnwhite99 <mnwhite@uchicago.edu>
2018-10-05 16:15:16 -0500

    Updated cond-loops.js
    Updated cond-loops.js to be more readable and accept different methods of changing the car speed.
    
    Previous version preserved as [OLD]-cond-loops.js.

--- grading-scripts/cond-loops.js
+++ grading-scripts/cond-loops.js
@@ -1,4 +1,44 @@
-// Be careful, shift() is actually editing car itself
+/// TODO: Put these in their own file
+
+/// Returns an array of the blocks in a script.
+function blocks(script) {
+    if (!script || script === undefined) return [];
+    return script[2];
+}
+
+/// Returns the opcode of a block.
+function opcode(block) {
+    if (!block || block === undefined || block.length === 0) return '';
+    return block[0];
+}
+
+/// Returns the first block in a script's opcode.
+function hatCode(script) {
+    return opcode(blocks(script)[0]);
+}
+
+/// Returns an array containing the subset of a sprite's scripts with a given event.
+function eventScripts(sprite, myEvent) {
+    if (!sprite.scripts) return [];
+    return sprite.scripts.filter(script => hatCode(script) === myEvent);
+}
+
+/// Returns an array containing the subset of a scripts's blocks with a given opcode.
+function opcodeBlocks(script, myOpcode) {
+    if (!blocks(script)) return [];
+    return blocks(script).filter(block => opcode(block) === myOpcode);
+}
+
+/// Returns true if a script contains a certain block, and false otherwise.
+function scriptContains(script, myOpcode) {
+    if (!script || script === undefined || !blocks(script)) return false;
+    return blocks(script).some(block => opcode(block) === myOpcode);
+}
+
+/// Finds a block with a given opcode in a script.
+function findBlock(script, myOpcode) {
+    return blocks(script).find(block => opcode(block) === myOpcode);
+}
 
 class GradeCondLoops {
 
@@ -25,77 +65,62 @@ class GradeCondLoops {
 
     grade(fileObj, user) {
         this.initReqs()
-
-        var car = fileObj.children.find((e) => {
-            return e.objName == 'Car'
-        })    
-        if(!car) return
-
-        this.checkSound(car)
-        this.checkSprites(fileObj.children)
-        this.checkCostume(car)
-        var event_script = this.checkStop(car)
-        var do_until_body = this.checkSay(event_script)
-        this.checkSpeed(do_until_body,.1)
-
+        var car = fileObj.children.find(child => child.objName === 'Car');
+        if(!car) return;
+        this.checkCostume(car);
+        this.checkSound(car);
+        this.checkSpeed(8, 0.1, car); // where to pull defaults from?
+        this.checkSprites(fileObj.children);
+        this.checkSay(this.checkStop(car));
     }
 
-    /* Check for car sound */
+    /// Checks that a different costume was chosen (requirement).
+    checkCostume(car) {
+        this.requirements.newCostume.bool = (car.currentCostumeIndex !== 2);
+    }
+
+    /// Checks that speed was changed (requirement).
+    checkSpeed(defaultSteps, defaultTime, car) {
+        for (var script of eventScripts(car, 'whenGreenFlag')) {
+            for (var block of opcodeBlocks(script, 'doUntil')) {
+                for (var subBlock of block[2]) {
+                    if (opcode(subBlock) === 'forward:') {
+                        if (subBlock[1] !== defaultSteps)
+                            this.requirements.changeSpeed.bool = true;
+                    }
+                    if (opcode(subBlock) === 'wait:elapsed:from:') {
+                        if (subBlock[1] !== defaultTime)
+                            this.requirements.changeSpeed.bool = true;
+                    }
+                }
+            }
+        }
+    }
+
+    /// Checks for car sound (extension).
     checkSound(car) {
-
-        this.extensions.carSound.bool = car.scripts.find((e) => {
-            return (undefined != e[2].find((f) => {
-                return f[0] == 'playSound:' 
-            }))
-        })
+        for (var script of car.scripts) {
+            if (scriptContains(script, 'playSound:'))
+                this.extensions.carSound.bool = true;
+        }
     }
 
+    /// Checks that other sprites perform actions (extension).
     checkSprites(sprites) {
         
         this.extensions.otherSprites.bool = sprites.find((e) => {
             if(e.objName != 'Car' && e.scripts) {
-                return e.scripts.find((f) => {
-                    return f[2].find((g) => {
-                        return g[0].startsWith('say') || g[0].startsWith('move')
+                return e.scripts.find((script) => {
+                    return blocks(script).find((block) => {
+                        return opcode(block).startsWith('say') ||
+                               opcode(block).startsWith('move')
                     })
                 })
             }
         })
     }
 
-    /* Check that speed was changed. */
-    checkSpeed(do_body, orig_wait, orig_move) {
-        try {do_body[2][0]}
-        catch(err) {return null}
- 
-        this.requirements.changeSpeed.bool = do_body[2].find((g) => {
-            if(g[0].startsWith("wait")) {
-                return (g[1] != orig_wait)
-            }
-        })
-    }
-
-    /* Check for say block, return doUntil scripts. */
-    checkSay(event_script) {
-        if(!event_script) return null
-        
-        while(event_script[0] && event_script[0][0] != 'doUntil')
-            event_script.shift()
-        var do_body = event_script.shift()
-
-        this.requirements.saySomething.bool = event_script.find((e) => {
-            return (e[0].startsWith('say') || e[0].startsWith('think'))
-        })
-
-        return do_body
-    }
-
-    /* Check for new costume. */
-    checkCostume(car) {
-        this.requirements.newCostume.bool = (car.currentCostumeIndex != 2)
-    }
-
-    /* Check for stop condition. */
+    /// Check for stop condition (requirement).
     checkStop(car) {
         var events_list = ['whenGreenFlag', 'whenKeyPressed','whenIReceive'];
         var event_script = null;
@@ -111,5 +136,16 @@ class GradeCondLoops {
             }
         });
         return event_script;
+    }
+
+    /// Checks that the car says something (requirement).
+    checkSay(script) {
+        if(!script) return null;      
+        var i = 0;
+        while(blocks(script)[i] && opcode(blocks(script)[i]) != 'doUntil') i++;
+        this.requirements.saySomething.bool = script.slice(i).find((block) => {
+            return (opcode(block).startsWith('say') || 
+            opcode(block).startsWith('think'))
+        });
     }
 }
