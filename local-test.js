13f98d73b9fe01c636d98d5f27ca56ecce1e2077
Elizabeth Crowdus <ecrowdus@cs.uchicago.edu>
2019-04-19 17:44:22 -0500

    scratch basics L1 and L2 now compatible with Scratch 3

---
+++ local-test.js
@@ -0,0 +1,55 @@
+/// Local tester for grading scripts
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+const fs     = require('fs');
+const AdmZip = require('adm-zip');
+
+if (process.argv.length < 4) { console.log('Usage: node local-test (path to grading script) (path to .sb3 file) [--verbose]'); return; }
+var graderPath  = process.argv[2];
+var projectPath = process.argv[3];
+var isVerbose   = process.argv.length > 4;
+
+var projectPathIsDirectory = fs.lstatSync(projectPath).isDirectory();
+
+if (projectPathIsDirectory) {
+    var fileNames = fs.readdirSync(projectPath);
+    for (var fileName of fileNames.filter(function(fileName) { return /_*\.sb3/g.test(fileName) } )) {
+        fullProjectPath = projectPath + '/' + fileName;
+        gradeProjectWithGrader(fullProjectPath, graderPath);
+    }
+}
+
+else /* if (!projectPathIsDirectory) */ {
+    gradeProjectWithGrader(projectPath, graderPath);
+}
+
+/// Helpers
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+function gradeProjectWithGrader(projectPath, graderPath) {
+
+    console.log('');
+
+    var GraderClass = require(graderPath);
+    var grader = new GraderClass();
+
+    var archive = new AdmZip(projectPath);
+    var projectJSON = JSON.parse(archive.readAsText('project.json'));
+    try {
+        console.log(projectPath);
+        if (isVerbose) console.log(projectJSON);
+        grader.grade(projectJSON, '');
+        console.log('Requirements:');
+        for (var item of grader.requirements) console.log(item.str + ': ' + ((item.bool) ? ('✔️') : ('❌')));
+        console.log('Extensions:');
+        for (var item of grader.extensions)   console.log(item.str + ': ' + ((item.bool) ? ('✔️') : ('❌')));
+    }
+    
+    catch(err) {
+        console.log('Error grading project');
+        if (isVerbose) console.error(err);
+    }
+
+    console.log('');
+
+}
