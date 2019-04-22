ed3642ea9169988c532515a18571b8dc0fe03a71
Elizabeth Crowdus <ecrowdus@cs.uchicago.edu>
2019-04-22 05:42:48 -0500

    events working

--- grading-scripts/events.js
+++ grading-scripts/events.js
@@ -1,6 +1,165 @@
 /// Grader for Events.Multicultural.L1
-/// Max White
-///////////////////////////////////////////////////////////////////////////////
+var sb3 = {
+    no: function(x) { //null checker
+        return (x == null || x == {} || x == undefined || !x || x == '' | x.length === 0);
+    },
+
+    jsonToSpriteBlocks: function(json, spriteName) { //retrieve a given sprite's blocks from JSON
+        if (this.no(json)) return []; //make sure script exists
+
+        var projInfo = json['targets'] //extract targets from JSON data
+        var allBlocks={};
+        var blocks={};
+        
+        //find sprite
+        for(i=0; i <projInfo.length; i++){
+            if(projInfo[i]['name'] == spriteName){
+                return projInfo[i]['blocks'];
+            }
+        }
+        return [];
+    }, //done
+    
+    jsonToSprite: function(json, spriteName) { //retrieve a given sprite's blocks from JSON
+        if (this.no(json)) return []; //make sure script exists
+
+        var projInfo = json['targets'] //extract targets from JSON data
+        
+        //find sprite
+        for(i=0; i <projInfo.length; i++){
+            if(projInfo[i]['name'] == spriteName){
+                return projInfo[i];
+            }
+        }
+        return [];
+    }, //done
+    
+    countSprites: function(json){
+        if (this.no(json)) return false; //make sure script exists
+        
+        var numSprites = 0;
+        var projInfo = json['targets'] //extract targets from JSON data
+        
+        for(i=0; i <projInfo.length; i++){
+            if(projInfo[i]['isStage'] == false){
+                numSprites ++;
+            }
+        }
+        return numSprites
+    },
+    
+    findSprite: function(json, spriteName){ //returns true if sprite with given name found
+        if (this.no(json)) return false; //make sure script exists
+
+        var projInfo = json['targets'] //extract targets from JSON data
+        
+        //find sprite
+        for(i=0; i <projInfo.length; i++){
+            if(projInfo[i]['name'] == spriteName){
+                return true;
+            }
+        }
+        return false;
+    }, //done
+    
+    findBlockID: function(blocks, opcode){
+        if(this.no(blocks) || blocks == {}) return null;
+        
+        for(block in blocks){ 
+            if(blocks[block]['opcode'] == opcode){
+                return block;
+            }
+        }
+        return null;
+    },
+    
+    findKeyPressID: function(blocks, key){
+        if(this.no(blocks) || blocks == {}) return null;
+        
+        for(block in blocks){ 
+            if(blocks[block]['opcode'] == 'event_whenkeypressed'){
+                if(blocks[block]['fields']['KEY_OPTION'][0] == key){
+                    return block;
+                }
+            }
+        }
+        return null;
+    },
+    
+    opcodeBlocks: function(script, myOpcode) { //retrieve blocks with a certain opcode from a script list of blocks
+        if (this.no(script)) return [];
+        
+        var miniscript = [];
+
+        for(block in script){
+            if(script[block]['opcode'] == myOpcode){
+                miniscript.push(script[block]);
+            }
+        }
+        return miniscript;
+    }, 
+    
+    opcode: function(block) { //retrives opcode from a block object 
+        if (this.no(block)) return "";
+        return block['opcode'];
+    }, 
+    
+    countBlocks: function(blocks,opcode){ //counts number of blocks with a given opcode
+        var total = 0;
+		for(id in blocks){ 
+            if([blocks][id]['opcode'] == opcode){
+                total = total + 1;
+            }
+        }
+        return total;
+    }, //done
+    
+    //given list of blocks, return a script
+    makeScript: function(blocks, blockID){
+        if (this.no(blocks) || this.no(blockID)) return [];
+        event_opcodes = ['event_whenflagclicked', 'event_whenthisspriteclicked','event_whenbroadcastreceived','event_whenkeypressed', 'event_whenbackdropswitchesto','event_whengreaterthan'];
+        
+        var curBlockID = blockID;
+        var script = {};
+    
+        while(curBlockID != null){
+            var curBlockInfo = blocks[curBlockID]; //Pull out info about the block
+            script[curBlockID]=curBlockInfo; //Add the block itself to the script dictionary DEBUG PUSH SITUATION
+            //Get parent info out
+            var parentID = curBlockInfo['parent']; //Block that comes before has key 'parent'
+            //parentInfo = blocks[parentID]
+    		var opcode = curBlockInfo['opcode'];
+
+            //If the block is not part of a script (i.e. it's the first block, but is not an event), return empty dictionary
+            if ((parentID == null) && !(event_opcodes.includes(opcode))){
+                return [];
+            }
+
+            //Iterate: set parent to curBlock
+            curBlockID = parentID
+        }
+        //Find all blocks that come after
+        curBlockID = blockID //Initialize with blockID of interest
+        while(curBlockID != null){
+            curBlockInfo = blocks[curBlockID]; //Pull out info about the block
+            script[curBlockID]=curBlockInfo; //Add the block itself to the script dictionary                
+            
+
+            //Get next info out
+            nextID = curBlockInfo['next']; //Block that comes after has key 'next'
+            //nextInfo = blocks[nextID]
+            opcode = curBlockInfo['opcode'];
+		
+            //If the block is not a script (i.e. it's an event but doesn't have anything after), return empty dictionary
+            if((nextID == null) && (event_opcodes.includes(opcode))){
+                return {};
+            }
+            //Iterate: Set next to curBlock
+            curBlockID = nextID;
+        }     
+        return script;
+    }
+};
 
 class GradeEvents {
 
@@ -12,146 +171,171 @@ class GradeEvents {
 
         this.requirements.containsThreeSprites =    /// contains >= 3 sprites
             {bool:false, str:'Project has at least three sprites.'};
-        /// Sprite 1
-        this.requirements.sprite1WhenClicked   =    /// sprite handles click
+        /// Sprite 1 aka Left
+        this.requirements.leftWhenClicked   =    /// sprite handles click
             {bool:false, str:'Sprite 1 handles click.'};
-        this.requirements.sprite1GetsBigger    =    /// sprite grows...
+        this.requirements.leftGetsBigger    =    /// sprite grows...
             {bool:false, str:'Sprite 1 gets bigger.'};
-        this.requirements.sprite1TalksTwice    =    /// then >= 2 say blocks...
+        this.requirements.leftTalksTwice    =    /// then >= 2 say blocks...
             {bool:false, str:'Then Sprite 1 has at least two say blocks.'};
-        this.requirements.sprite1ResetsSize    =    /// then resets size
+        this.requirements.leftResetsSize    =    /// then resets size
             {bool:false, str:'The Sprite 1 resets size.'};
-        /// Sprite 2
-        this.requirements.sprite2WhenClicked   =    /// ditto
+        /// Sprite 2 aka middle
+        this.requirements.middleWhenClicked   =    /// ditto
             {bool:false, str:'Sprite 2 handles click.'};
-        this.requirements.sprite2GetsBigger    =
+        this.requirements.middleGetsBigger    =
             {bool:false, str:'Sprite 2 gets bigger.'};
-        this.requirements.sprite2TalksTwice    =
+        this.requirements.middleTalksTwice    =
             {bool:false, str:'Then Sprite 2 has at least two say blocks.'};
-        this.requirements.sprite2ResetsSize    =
+        this.requirements.middleResetsSize    =
             {bool:false, str:'The Sprite 2 resets size.'};
-        /// Sprite 3
-        this.requirements.sprite3WhenClicked   =
+        /// Sprite 3 aka right
+        this.requirements.rightWhenClicked   =
             {bool:false, str:'Sprite 3 handles click.'};
-        this.requirements.sprite3GetsBigger    =
+        this.requirements.rightGetsBigger    =
             {bool:false, str:'Sprite 3 gets bigger.'};
-        this.requirements.sprite3TalksTwice    =
+        this.requirements.rightTalksTwice    =
         {bool:false, str:'Then Sprite 3 has at least two say blocks.'};
-        this.requirements.sprite3ResetsSize    = 
+        this.requirements.rightResetsSize    = 
             {bool:false, str:'The Sprite 3 resets size.'};
-
-    }
-
-    checkScripts(sprite) {
-        
-        var finalResults         = {};
-        finalResults.whenClicked =
-        finalResults.getsBigger  =
-        finalResults.resetsSize  =
-        finalResults.talksTwice  = false;
-        finalResults.score       = -1;
-        if (!sprite.hasOwnProperty('scripts')) return finalResults;
-
-        for (var script of sprite.scripts) {
-
-            /// Initialize for each script
-            var results         = {};
-            results.whenClicked =
-            results.getsBigger  =
-            results.resetsSize  =
-            results.talksTwice  = false;
-            results.score       = 0;
-
-            var initialSize = sprite.scale;
-            var newSize     = initialSize;
-            var sayCount    = 0;
-
-            /// Check script if it has 'whenClicked' event
-            if (script.length > 1 && script[2][0][0] === 'whenClicked') {
-                results.whenClicked = true;
-                for (var block of script[2]) {
-
-                    var blockName = block[0];
-
-                    /// Check for changing size
-                    if (blockName === 'changeSizeBy:')
-                        newSize += block[1] / 100;
-
-                    if (blockName === 'setSizeTo:')
-                        newSize = block[1] / 100;
-
-                    if (newSize > initialSize)
-                        results.getsBigger = true;
-
-                    if (results.getsBigger && newSize === initialSize)
-                        results.resetsSize = true;
-
-                    /// Count say blocks in script WHILE sprite is bigger
-                    if (results.getsBigger  &&
-                        !results.resetsSize &&
-                        ['say:',
-                         'say:duration:elapsed:from:'].includes(blockName))
-                        sayCount++;
-                }
-            }
-
-            /// Summarize results into object      
-            results.talksTwice = sayCount > 1;
-            results.score = results.getsBigger +
-                            results.resetsSize +
-                            results.talksTwice +
-                            results.whenClicked;
-
-            if (results.score > finalResults.score) finalResults = results;
-        }
-
-        /// Return results from most complete script          
-        return finalResults;
 
     }
 
     grade(fileObj, user) {
 
         this.initReqs();
-        this.requirements.containsThreeSprites.bool = fileObj.info.spriteCount > 2;
-
-        /// Build array of results objects
-        var resultsArr = [];
-        for (var child of fileObj.children) {
-            if (child.hasOwnProperty('objName')) {
-                var results = this.checkScripts(child);
-                resultsArr.push(results);
+        
+        //check number of sprites
+        if(sb3.countSprites(fileObj) > 2){
+            this.requirements.containsThreeSprites.bool = true; 
+        }
+        
+        for(var i in fileObj['targets']){ //find sprite
+            var sprite = fileObj['targets'][i]
+            if(sprite['name'] == 'Right'){
+                var right = sprite;
+            }
+            if(sprite['name'] == 'Middle'){
+                var middle = sprite;
+            }
+            if(sprite['name'] == 'Left'){
+                var left = sprite;
             }
         }
-
-        /// Sort resultsArr to display the most complete scripts first
-        resultsArr.sort(function(a, b) {
-            return b.score - a.score;
-        })
-
-        /// Pull requirements from head of resultsArr
-
-        /// Sprite 1
-        if(resultsArr[0]) {
-            this.requirements.sprite1WhenClicked.bool = resultsArr[0].whenClicked;
-            this.requirements.sprite1GetsBigger.bool  = resultsArr[0].getsBigger;
-            this.requirements.sprite1TalksTwice.bool  = resultsArr[0].talksTwice;
-            this.requirements.sprite1ResetsSize.bool  = resultsArr[0].resetsSize;
+        
+        //check Left sprite
+        var leftid = sb3.findBlockID(left['blocks'], 'event_whenthisspriteclicked');
+        if(leftid != null){
+            this.requirements.leftWhenClicked.bool = true;
+            var lefttalkcount = 0;
+            var leftScript = sb3.makeScript(left['blocks'], leftid);
+            for(var i in leftScript){
+                //change size, case 1
+                if(leftScript[i]['opcode'] == 'looks_changesizeby'){
+                    console.log(leftScript[i]['inputs'])
+                    if(leftScript[i]['inputs']['CHANGE'][1][1] >= 1 ){
+                        this.requirements.leftGetsBigger.bool = true;
+                        var leftchange = leftScript[i]['inputs']['CHANGE'][1][1]
+                       }
+                }  
+                //change size, case 2
+                if(leftScript[i]['opcode'] == 'looks_setsizeto' && leftScript[i]['inputs']['CHANGE'][1][1] >= 100 ){
+                    this.requirements.leftGetsBigger.bool = true
+                    var leftset = leftScript[i]['inputs']['CHANGE'][1][1]
+                } 
+                //talks twice
+                if(leftScript[i]['opcode'] == 'looks_sayforsecs'){
+                    lefttalkcount ++;
+                    if(lefttalkcount >= 2){
+                        this.requirements.leftTalksTwice.bool = true;
+                    }
+                }
+                //size reset, case 1
+                if(leftScript[i]['opcode'] == 'looks_changesizeby' && leftset != undefined && (leftset - leftScript[i]['inputs']['CHANGE'][1][1]) == 0){
+                    this.requirements.leftResetsSize.bool = true;
+                }  
+                //size reset, case 2
+                if(leftScript[i]['opcode'] == 'looks_setsizeto' && leftchange != undefined && (leftchange - leftScript[i]['inputs']['CHANGE'][1][1]) == 0){
+                    this.requirements.leftResetsSize.bool = true
+                } 
+            }
         }
-        /// Sprite 2
-        if(resultsArr[1]) {
-            this.requirements.sprite2WhenClicked.bool = resultsArr[1].whenClicked;
-            this.requirements.sprite2GetsBigger.bool  = resultsArr[1].getsBigger;
-            this.requirements.sprite2TalksTwice.bool  = resultsArr[1].talksTwice;
-            this.requirements.sprite2ResetsSize.bool  = resultsArr[1].resetsSize;
-        }
-        /// Sprite 3
-        if(resultsArr[2]) {
-            this.requirements.sprite3WhenClicked.bool = resultsArr[2].whenClicked;
-            this.requirements.sprite3GetsBigger.bool  = resultsArr[2].getsBigger;
-            this.requirements.sprite3TalksTwice.bool  = resultsArr[2].talksTwice;
-            this.requirements.sprite3ResetsSize.bool  = resultsArr[2].resetsSize;
-        }
-
+        
+        //check Middle sprite
+        var middleid = sb3.findBlockID(middle['blocks'], 'event_whenthisspriteclicked');
+        if(middleid != null ){
+            this.requirements.middleWhenClicked.bool = true;
+            var middletalkcount = 0;
+            var middleScript = sb3.makeScript(middle['blocks'], middleid);
+            for(var i in middleScript){
+                //change size, case 1
+                if(middleScript[i]['opcode'] == 'looks_changesizeby'){
+                    console.log(middleScript[i]['inputs'])
+                    if(middleScript[i]['inputs']['CHANGE'][1][1] >= 1 ){
+                        this.requirements.middleGetsBigger.bool = true;
+                        var middlechange = middleScript[i]['inputs']['CHANGE'][1][1]
+                       }
+                }  
+                //change size, case 2
+                if(middleScript[i]['opcode'] == 'looks_setsizeto' && middleScript[i]['inputs']['CHANGE'][1][1] >= 100 ){
+                    this.requirements.middleGetsBigger.bool = true
+                    var middleset = middleScript[i]['inputs']['CHANGE'][1][1]
+                } 
+                //talks twice
+                if(middleScript[i]['opcode'] == 'looks_sayforsecs'){
+                    middletalkcount ++;
+                    if(middletalkcount >= 2){
+                        this.requirements.middleTalksTwice.bool = true;
+                    }
+                }
+                //size reset, case 1
+                if(middleScript[i]['opcode'] == 'looks_changesizeby' && middleset != undefined && (middleset - middleScript[i]['inputs']['CHANGE'][1][1]) == 0){
+                    this.requirements.middleResetsSize.bool = true;
+                }  
+                //size reset, case 2
+                if(middleScript[i]['opcode'] == 'looks_setsizeto' && middlechange != undefined && (middlechange - middleScript[i]['inputs']['CHANGE'][1][1]) == 0){
+                    this.requirements.middleResetsSize.bool = true
+                } 
+            }
+        }  
+        
+        //check Right sprite
+        var rightid = sb3.findBlockID(right['blocks'], 'event_whenthisspriteclicked');
+        if(rightid != null){
+            this.requirements.rightWhenClicked.bool = true;
+            var righttalkcount = 0;
+            var rightScript = sb3.makeScript(right['blocks'], rightid);
+            for(var i in rightScript){
+                //change size, case 1
+                if(rightScript[i]['opcode'] == 'looks_changesizeby'){
+                    if(rightScript[i]['inputs']['CHANGE'][1][1] >= 1 ){
+                        this.requirements.rightGetsBigger.bool = true;
+                        var rightchange = rightScript[i]['inputs']['CHANGE'][1][1]
+                       }
+                }  
+                //change size, case 2
+                if(rightScript[i]['opcode'] == 'looks_setsizeto' && rightScript[i]['inputs']['CHANGE'][1][1] >= 100 ){
+                    this.requirements.rightGetsBigger.bool = true
+                    var rightset = rightScript[i]['inputs']['CHANGE'][1][1]
+                } 
+                //talks twice
+                if(rightScript[i]['opcode'] == 'looks_sayforsecs'){
+                    righttalkcount ++;
+                    if(righttalkcount >= 2){
+                        this.requirements.rightTalksTwice.bool = true;
+                    }
+                }
+                //size reset, case 1
+                if(rightScript[i]['opcode'] == 'looks_changesizeby' && rightset != undefined && (rightset - rightScript[i]['inputs']['CHANGE'][1][1]) == 0){
+                    this.requirements.rightResetsSize.bool = true;
+                }  
+                //size reset, case 2
+                if(rightScript[i]['opcode'] == 'looks_setsizeto' && rightchange != undefined && (rightchange - rightScript[i]['inputs']['CHANGE'][1][1]) == 0){
+                    this.requirements.rightResetsSize.bool = true
+                } 
+            }
+        } 
     }
 }
+
+module.exports = GradeEvents;
