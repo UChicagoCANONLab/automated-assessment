4aa454d6ba06aa677f77f97b1c0f02370fab183a
Elizabeth Crowdus <ecrowdus@cs.uchicago.edu>
2019-04-21 21:32:42 -0500

    TODO animation extensions

--- grading-scripts/animation.js
+++ grading-scripts/animation.js
@@ -1,4 +1,164 @@
+var sb3 = {
+    no: function(x) { //null checker
+        return (x == null || x == {} || x == undefined || !x || x == '' | x.length === 0);
+    },
 
+    jsonToSpriteBlocks: function(json, spriteName) { //retrieve a given sprite's blocks from JSON
+        if (this.no(json)) return []; //make sure script exists
+
+        var projInfo = json['targets'] //extract targets from JSON data
+        var allBlocks={};
+        var blocks={};
+        
+        //find sprite
+        for(i=0; i <projInfo.length; i++){
+            if(projInfo[i]['name'] == spriteName){
+                return projInfo[i]['blocks'];
+            }
+        }
+        return [];
+    }, //done
+    
+    jsonToSprite: function(json, spriteName) { //retrieve a given sprite's blocks from JSON
+        if (this.no(json)) return []; //make sure script exists
+
+        var projInfo = json['targets'] //extract targets from JSON data
+        
+        //find sprite
+        for(i=0; i <projInfo.length; i++){
+            if(projInfo[i]['name'] == spriteName){
+                return projInfo[i];
+            }
+        }
+        return [];
+    }, //done
+    
+    countSprites: function(json){
+        if (this.no(json)) return false; //make sure script exists
+        
+        var numSprites = 0;
+        var projInfo = json['targets'] //extract targets from JSON data
+        
+        for(i=0; i <projInfo.length; i++){
+            if(projInfo[i]['isStage'] == false){
+                numSprites ++;
+            }
+        }
+        return numSprites
+    },
+    
+    findSprite: function(json, spriteName){ //returns true if sprite with given name found
+        if (this.no(json)) return false; //make sure script exists
+
+        var projInfo = json['targets'] //extract targets from JSON data
+        
+        //find sprite
+        for(i=0; i <projInfo.length; i++){
+            if(projInfo[i]['name'] == spriteName){
+                return true;
+            }
+        }
+        return false;
+    }, //done
+    
+    findBlockID: function(blocks, opcode){
+        if(this.no(blocks) || blocks == {}) return null;
+        
+        for(block in blocks){ 
+            if(blocks[block]['opcode'] == opcode){
+                return block;
+            }
+        }
+        return null;
+    },
+    
+    findKeyPressID: function(blocks, key){
+        if(this.no(blocks) || blocks == {}) return null;
+        
+        for(block in blocks){ 
+            if(blocks[block]['opcode'] == 'event_whenkeypressed'){
+                if(blocks[block]['fields']['KEY_OPTION'][0] == key){
+                    return block;
+                }
+            }
+        }
+        return null;
+    },
+    
+    opcodeBlocks: function(script, myOpcode) { //retrieve blocks with a certain opcode from a script list of blocks
+        if (this.no(script)) return [];
+        
+        var miniscript = [];
+
+        for(block in script){
+            if(script[block]['opcode'] == myOpcode){
+                miniscript.push(script[block]);
+            }
+        }
+        return miniscript;
+    }, 
+    
+    opcode: function(block) { //retrives opcode from a block object 
+        if (this.no(block)) return "";
+        return block['opcode'];
+    }, 
+    
+    countBlocks: function(blocks,opcode){ //counts number of blocks with a given opcode
+        var total = 0;
+		for(id in blocks){ 
+            if([blocks][id]['opcode'] == opcode){
+                total = total + 1;
+            }
+        }
+        return total;
+    }, //done
+    
+    //given list of blocks, return a script
+    makeScript: function(blocks, blockID){
+        if (this.no(blocks) || this.no(blockID)) return [];
+        event_opcodes = ['event_whenflagclicked', 'event_whenthisspriteclicked','event_whenbroadcastreceived','event_whenkeypressed', 'event_whenbackdropswitchesto','event_whengreaterthan'];
+        
+        var curBlockID = blockID;
+        var script = {};
+    
+        while(curBlockID != null){
+            var curBlockInfo = blocks[curBlockID]; //Pull out info about the block
+            script[curBlockID]=curBlockInfo; //Add the block itself to the script dictionary DEBUG PUSH SITUATION
+            //Get parent info out
+            var parentID = curBlockInfo['parent']; //Block that comes before has key 'parent'
+            //parentInfo = blocks[parentID]
+    		var opcode = curBlockInfo['opcode'];
+
+            //If the block is not part of a script (i.e. it's the first block, but is not an event), return empty dictionary
+            if ((parentID == null) && !(event_opcodes.includes(opcode))){
+                return [];
+            }
+
+            //Iterate: set parent to curBlock
+            curBlockID = parentID
+        }
+        //Find all blocks that come after
+        curBlockID = blockID //Initialize with blockID of interest
+        while(curBlockID != null){
+            curBlockInfo = blocks[curBlockID]; //Pull out info about the block
+            script[curBlockID]=curBlockInfo; //Add the block itself to the script dictionary                
+            
+
+            //Get next info out
+            nextID = curBlockInfo['next']; //Block that comes after has key 'next'
+            //nextInfo = blocks[nextID]
+            opcode = curBlockInfo['opcode'];
+		
+            //If the block is not a script (i.e. it's an event but doesn't have anything after), return empty dictionary
+            if((nextID == null) && (event_opcodes.includes(opcode))){
+                return {};
+            }
+            //Iterate: Set next to curBlock
+            curBlockID = nextID;
+        }     
+        return script;
+    }
+};
 
 class GradeAnimation {
 
@@ -25,6 +185,8 @@ class GradeAnimation {
         this.extensions.extraPostFinishAnimation = {bool:false, str:'Bee changes costume during victory dance.'};;
         this.extensions.extraTurnBlock = {bool:false, str:'Bee uses turn block during victory dance.'};;
     }
+    
+
 
     grade(fileObj, user) {
         this.initReqs();
@@ -32,10 +194,9 @@ class GradeAnimation {
         var that = this;
 
         // general metrics
-        var sprites = fileObj.children;
-        var validMoves = ['gotoX:y:', 'changeXposBy:', 'changeYposBy:', 'forward:', 'glideSecs:toX:y:elapsed:from:'];
-        var validLoops = ['doForever', 'doRepeat', 'doUntil'];
-        var validCostumes = ['lookLike:', 'nextCostume'];
+        var validMoves = ['motion_gotoxy', 'motion_changexby', 'motion_changeyby', 'motion_movesteps', 'motion_glidesecstoxy'];
+        var validLoops = ['control_forever', 'control_repeat', 'control_repeat_until'];
+        var validCostumes = ['looks_switchcostumeto', 'looks_nextcostume'];
 
         var extraBeeWaitTime = -1;
         var extraBeeMoveSteps = -1;
@@ -43,172 +204,62 @@ class GradeAnimation {
         var extraSnakeMoveSteps = -1;
         var winner = null;
         var victoryDance = {};
-
-        // check the script for the Bee sprite
-        var checkScript = function(sprite) {
-            for (var i = 0; i < sprite.scripts.length; i++) {
-                var script = sprite.scripts[i][2];
-                var eventName = script[0][0];
-                if (script.length > 1) {
-                    if (eventName === 'whenKeyPressed') {
-                        var eventKey = script[0][1];
-                        if (eventKey === 'down arrow') {
-                            // check correctness for Bee, skip for Snake
-                            if (sprite.objName === 'Bee') {
-                                that.requirements.handlesDownArrow.bool = true;
-                                checkDownArrow(script);
-                            }
-                        } else if (eventKey === 'space') {
-                            // check correctness for Bee, record speed for Snake
-                            if (sprite.objName === 'Bee') {
-                                that.requirements.handlesSpaceBar.bool = true;
-                                checkSpaceBar(script);
-                                that.extensions.extraTurnBlock.bool = that.extensions.extraTurnBlock.bool && (sprite.rotationStyle === 'normal');
-                            } else if (sprite.objName === 'Snake') {
-                                checkSpaceBar(script, false);
-                            }
-                        }
-                    }
-                }
-            }
-        };
-
-        // check the blocks following Down Arrow
-        // costume change, wait
-        var checkDownArrow = function(script) {
-            var blocks = script.slice(1);
-            var waitBlockCounter = 0;
-            var switchBlockCounter = 0;
-            for (var i = 0; i < blocks.length; i++) {
-                var block = blocks[i];
-                if (block[0] === 'wait:elapsed:from:') 
-                    waitBlockCounter += 1;
-                if (validCostumes.includes(block[0])) {
-                    switchBlockCounter += 1;
-                }
-                if (validLoops.includes(block[0])) {
-                    if (block[1] >= 4) {
-                        var nested = block.slice(2)[0];
-                        for (var i = 0; i < nested.length; i++) {
-                            if (nested[i][0] === 'wait:elapsed:from:') 
-                                that.requirements.downArrowWaitBlock.bool = true;
-                            if (nested[i][0] === 'nextCostume')
-                                that.requirements.downArrowCostumeChange.bool = true;
-                        }
-                    }
-                }
-            }
-            // showing all 4 costumes, may include beginning
-            if (waitBlockCounter >= 3)
-                that.requirements.downArrowWaitBlock.bool = true;
-            if (switchBlockCounter >= 3)
-                that.requirements.downArrowCostumeChange.bool = true;          
-        };
-
-        // check the blocks following Space Bar
-        // loop, movement, costume change, wait
-        var checkSpaceBar = function(script, grading=true) {
-            var blocks = script.slice(1);
-            var loopBlockIndex = -1;
-            for (var i = 0; i < blocks.length; i++) {
-                var block = blocks[i];
-                // requirements: racing loop
-                if (i === 0 && (validLoops.includes(block[0]))) {
-                    loopBlockIndex = i;
-                    if (grading && block[0] !== 'doForever')
-                        that.requirements.spaceBarLoop.bool = true;
-                    var nested;
-                    if (block[0] === 'doForever') 
-                        nested = block.slice(1)[0];
-                    else
-                        nested = block.slice(2)[0];
-                    for (var j = 0; j < nested.length; j++) {
-                        if (grading) {
-                            // requirements
-                            if (nested[j][0] === 'wait:elapsed:from:') {
-                                that.requirements.spaceBarWaitBlock.bool = true;
-                                if (extraBeeWaitTime === -1)
-                                    extraBeeWaitTime = 0;
-                                extraBeeWaitTime += nested[j][1];
-                            }
-                            if (nested[j][0] === 'forward:') {
-                                that.requirements.spaceBarMovement.bool = true;
-                                if (extraBeeMoveSteps === -1)
-                                    extraBeeMoveSteps = 0;
-                                extraBeeMoveSteps += nested[j][1];
-                            }
-                            if (nested[j][0] === 'nextCostume')
-                                that.requirements.spaceBarCostumeChange.bool = true;
-                            // extra: wiggly path for Bee
-                            if (nested[j][0] === 'turnLeft:' || nested[j][0] === 'turnRight:')
-                                that.extensions.extraTurnBlock.bool = true;
-                        } else {
-                            if (nested[j][0] === 'wait:elapsed:from:')
-                                extraSnakeWaitTime = nested[j][1];
-                            if (nested[j][0] === 'forward:')
-                                extraSnakeMoveSteps = nested[j][1];
-                        }
-                    }
-                }
-                // extra: victory dance after racing loop
-                else {
-                    var checkDance = function(block) {
-                        var extraCostumeChange = false;
-                        var extraMoveBlock = false;
-                        if (block[0] === 'doRepeat' || block[0] === 'forever') {
-                            var nested = block.slice(2)[0];
-                            for (var j = 0; j < nested.length; j++) {
-                                if (validMoves.includes(nested[j][0]))
-                                    extraMoveBlock = true;
-                                if (nested[j][0] === 'lookLike:' || nested[j][0] === 'nextCostume')
-                                    extraCostumeChange = true;
-                            }
-                        } else if (validMoves.includes(block[0])) {
-                            extraMoveBlock = true;
-                        }
-                        return (extraMoveBlock || extraCostumeChange);
-                    };
-                    if (loopBlockIndex !== -1 && i > loopBlockIndex) {
-                        if (grading) {
-                            victoryDance.bee = checkDance(block);
-                        }
-                        else {
-                            victoryDance.snake = checkDance(block);
-                        }
-                    };               
-                }
-            }
-        };
-
-        // main
-        // check script for both Bee and Snake
-        for (var i = 0; i < sprites.length; i++) {
-            var sprite = sprites[i];
-            if (sprite.hasOwnProperty('objName') && 
-                sprite.hasOwnProperty('scripts')) {
-                checkScript(sprite);
+        
+        for(var i in fileObj['targets']){ //find bee
+            var bee = fileObj['targets'][i]
+            if(bee['name'] == 'Bee'){
+                break;
             }
         }
+        
+        //make space key script
+        var keyid = sb3.findKeyPressID(bee['blocks'], 'space');
+        if(keyid != null){
+            this.requirements.handlesSpaceBar.bool = true;
+            var spaceScript = sb3.makeScript(bee['blocks'], keyid);
+            for(var i in spaceScript){
+                //check for loop
+                if(validLoops.includes(spaceScript[i]['opcode'])){
+                    this.requirements.spaceBarLoop.bool = true;
+                }  
+                //check for wait block
+                if(spaceScript[i]['opcode'] == 'control_wait'){
+                    this.requirements.spaceBarWaitBlock.bool = true;
+                }
+                //check for movement 
+                if(validMoves.includes(spaceScript[i]['opcode'])){
+                    this.requirements.spaceBarMovement.bool = true;
+                }
+                //check for costumes
+                if(validCostumes.includes(spaceScript[i]['opcode'])){
+                    this.requirements.spaceBarCostumeChange.bool = true;
+                }
+            }
+        }
+        
+        
+        //make down arrow script
+        var arrowid = sb3.findKeyPressID(bee['blocks'], 'down arrow');
+        if(arrowid != null){
+            this.requirements.handlesDownArrow.bool = true;
+            var arrowScript = sb3.makeScript(bee['blocks'], arrowid);
+            for(var i in arrowScript){
+                //check for costume change
+                if(arrowScript[i]['opcode'] == 'looks_switchcostumeto' || arrowScript[i]['opcode'] == 'looks_nextcostume'){
+                    this.requirements.downArrowCostumeChange.bool = true;
+                }  
+                //check for wait block
+                if(arrowScript[i]['opcode'] == 'control_wait'){
+                    this.requirements.downArrowWaitBlock.bool = true;
+                }
+            }
+        }c
 
-        // extra: Bee and Snake finish with different speed
-        if (extraBeeWaitTime !== -1 && extraBeeMoveSteps !== -1) {
-            if (extraBeeWaitTime !== extraSnakeWaitTime) {
-                this.requirements.hasWinner.bool = true;
-                if (extraBeeWaitTime < extraSnakeWaitTime)
-                    winner = 'bee';
-                else
-                    winner = 'snake';
-            }
-            if (extraBeeMoveSteps !== extraSnakeMoveSteps) {
-                 this.requirements.hasWinner.bool = true;
-                if (extraBeeMoveSteps > extraSnakeMoveSteps)
-                    winner = 'bee';
-                else
-                    winner = 'snake';
-            }
-        }
-        if (winner && victoryDance.hasOwnProperty(winner)) {
-            this.extensions.extraPostFinishAnimation.bool = true;
-        }
     }
+    
+    
+    
+
+
 }
+module.exports = GradeAnimation;
