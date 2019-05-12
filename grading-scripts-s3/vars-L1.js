49f218a78f8aca4abb11cbe02d46530bebcefe05
Elizabeth Crowdus <ecrowdus@cs.uchicago.edu>
2019-05-11 20:16:37 -0500

    TODO variables L1 extensions

--- grading-scripts-s3/vars-L1.js
+++ grading-scripts-s3/vars-L1.js
@@ -383,7 +383,7 @@ class GradeVariablesL1 {
 
     }
     
-    checkAddv(ben, opblock){
+    checkAdd(ben, opblock){
         var aID = opblock['inputs']['NUM1'][1]
         var a = ben['blocks'][aID]
         var bID = opblock['inputs']['NUM2'][1]
