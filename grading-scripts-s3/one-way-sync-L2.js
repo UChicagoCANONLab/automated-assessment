01e791e2d24e30b2bd2c809557a1af11794a5da2
Marco Anaya <marco.a.anaya34@gmail.com>
2019-08-16 10:46:57 -0500

    finished updating one-way-sync L2

--- grading-scripts-s3/one-way-sync-L2.js
+++ grading-scripts-s3/one-way-sync-L2.js
@@ -1,397 +1,110 @@
 /* One Way Sync L2 Autograder
- * Marco Anaya, Spring 2019
+ * Marco Anaya, Summer 2019
  */
-var sb3 = {
-	//null checker
-	no: function(x) { 
-	  return (x == null || x == {} || x == undefined || !x || x == '' | x.length === 0);
-	},
 
-	//retrieve a given sprite's blocks from JSON
-	//note: doesn't check whether or not blocks are properly attached
-	jsonToSpriteBlocks: function(json, spriteName) { 
-		if (this.no(json)) return []; //make sure script exists
+require('./scratch3');
 
-		var projInfo = json['targets'] //extract targets from JSON data
-		var allBlocks={};
-		var blocks={};
-		
-		//find sprite
-		for(i=0; i <projInfo.length; i++){
-			if(projInfo[i]['name'] == spriteName){
-				return projInfo[i]['blocks'];
-			}
-		}
-		return [];
-	}, //done
-	
-	//retrieve a given sprite's info (not just blocks) from JSON
-	jsonToSprite: function(json, spriteName) { 
-		if (this.no(json)) return []; //make sure script exists
+module.exports = class {
 
-		var projInfo = json['targets'] //extract targets from JSON data
-		
-		//find sprite
-		for(i=0; i <projInfo.length; i++){
-			if(projInfo[i]['name'] == spriteName){
-				return projInfo[i];
-			}
-		}
-		return [];
-	}, //done
-	
-	//counts the number of non-background sprites in a project
-	countSprites: function(json){
-		if (this.no(json)) return false; //make sure script exists
-		
-		var numSprites = 0;
-		var projInfo = json['targets'] //extract targets from JSON data
-		
-		for(i=0; i <projInfo.length; i++){
-			if(projInfo[i]['isStage'] == false){
-				numSprites ++;
-			}
-		}
-		return numSprites
-	},
-	
-	//looks through json to see if a sprite with a given name is present
-	//returns true if sprite with given name found
-	findSprite: function(json, spriteName){ 
-		if (this.no(json)) return false; //make sure script exists
-
-		var projInfo = json['targets'] //extract targets from JSON data
-		
-		//find sprite
-		for(i=0; i <projInfo.length; i++){
-			if(projInfo[i]['name'] == spriteName){
-				return true;
-			}
-		}
-		return false;
-	}, //done
-	
-	//returns list of block ids given a set of blocks
-	findBlockIDs: function(blocks, opcode){
-		if(this.no(blocks) || blocks == {}) return null;
-		
-		var blockids = [];
-		
-		for(block in blocks){ 
-			if(blocks[block]['opcode'] == opcode){
-				blockids.push(block);
-			}
-		}
-		return blockids;
-	},
-	
-	//given particular key, returns list of block ids of a certain kind of key press given a set of blocks 
-	findKeyPressID: function(blocks, key){
-		if(this.no(blocks) || blocks == {}) return [];
-		
-		var blockids = [];
-		
-		for(block in blocks){ 
-			if(blocks[block]['opcode'] == 'event_whenkeypressed'){
-				if(blocks[block]['fields']['KEY_OPTION'][0] == key){
-					blockids.push(block);
-				}
-			}
-		}
-		return blockids;
-	},
-	
-	opcodeBlocks: function(script, myOpcode) { //retrieve blocks with a certain opcode from a script list of blocks
-		if (this.no(script)) return [];
-		
-		var miniscript = [];
-
-		for(block in script){
-			if(script[block]['opcode'] == myOpcode){
-				miniscript.push(script[block]);
-			}
-		}
-		return miniscript;
-	}, 
-	
-	opcode: function(block) { //retrives opcode from a block object 
-		if (this.no(block)) return "";
-		return block['opcode'];
-	}, 
-	
-	countBlocks: function(blocks,opcode){ //counts number of blocks with a given opcode
-		var total = 0;
-	for(id in blocks){ 
-			if([blocks][id]['opcode'] == opcode){
-				total = total + 1;
-			}
-		}
-		return total;
-	}, //done
-	
-	//(recursive) helper function to extract blocks inside a given loop
-	//works like makeScript except it only goes down the linked list (rather than down & up)
-	loopExtract: function(blocks, blockID){
-		if (this.no(blocks) || this.no(blockID)) return [];
-		loop_opcodes = ['control_repeat', 'control_forever', 'control_if', 'control_if_else', 'control_repeat_until'];
-		
-		var curBlockID = blockID;
-		var script = [];
-
-		//Find all blocks that come after
-		curBlockID = blockID //Initialize with blockID of interest
-		while(curBlockID != null){
-			curBlockInfo = blocks[curBlockID]; //Pull out info about the block
-			script.push(curBlockInfo); //Add the block itself to the script dictionary                
-
-
-			//nextInfo = blocks[nextID]
-			opcode = curBlockInfo['opcode'];
-			
-			//extract nested children if loop block
-			if(loop_opcodes.includes(opcode)){
-				var innerloop = curBlockInfo['inputs']['SUBSTACK'][1]
-				if(innerloop != undefined){
-					var nested_blocks = this.makeScript(blocks, innerloop)
-					for(b in nested_blocks){
-						script.push(nested_blocks[b])
-					}
-				}
-			}
-
-			//Get next info out
-			nextID = curBlockInfo['next']; //Block that comes after has key 'next'
-	
-			//If the block is not a script (i.e. it's an event but doesn't have anything after), return empty dictionary
-			if((nextID == null) && (event_opcodes.includes(opcode))){
-				return [];
-			}
-			//Iterate: Set next to curBlock
-			curBlockID = nextID;
-		}     
-		return script;        
-	},
-	
-	//given list of blocks and a keyID of a block, return a script
-	makeScript: function(blocks, blockID){
-		if (this.no(blocks) || this.no(blockID)) return [];
-		event_opcodes = ['event_whenflagclicked', 'event_whenthisspriteclicked','event_whenbroadcastreceived','event_whenkeypressed', 'event_whenbackdropswitchesto','event_whengreaterthan'];
-		loop_opcodes = ['control_repeat', 'control_forever', 'control_if', 'control_if_else', 'control_repeat_until'];
-		
-		var curBlockID = blockID;
-		var script = [];
-	
-		//find all blocks that come before
-		while(curBlockID != null){
-			var curBlockInfo = blocks[curBlockID]; //Pull out info about the block
-			script.push(curBlockInfo); //Add the block itself to the script dictionary 
-			
-			//Get parent info out
-			var parentID = curBlockInfo['parent']; //Block that comes before has key 'parent'
-			//parentInfo = blocks[parentID]
-		var opcode = curBlockInfo['opcode'];
-
-			
-			//extract nested children if loop block
-			if(loop_opcodes.includes(opcode)){
-				var innerloop = curBlockInfo['inputs']['SUBSTACK'][1]
-				if(innerloop != undefined){
-					var nested_blocks = this.loopExtract(blocks, innerloop)
-					for(b in nested_blocks){
-						script.push(nested_blocks[b])
-					}
-				}
-			}
-			
-			//If the block is not part of a script (i.e. it's the first block, but is not an event), return empty dictionary
-			if ((parentID == null) && !(event_opcodes.includes(opcode))){
-				return [];
-			}
-
-			//Iterate: set parent to curBlock
-			curBlockID = parentID
-		}
-
-		//find all blocks that come after
-		curBlockID = blocks[blockID]['next']
-		while(curBlockID != null){
-			curBlockInfo = blocks[curBlockID]; //Pull out info about the block
-
-
-			//nextInfo = blocks[nextID]
-			opcode = curBlockInfo['opcode'];
-			
-			//extract nested children if loop block
-			if(loop_opcodes.includes(opcode)){
-				var innerloop = curBlockInfo['inputs']['SUBSTACK'][1]
-				if(innerloop != undefined){
-					var nested_blocks = this.loopExtract(blocks, innerloop)
-					for(b in nested_blocks){                            
-						script.push(nested_blocks[b])
-					}
-				}
-			}
-	
-			//Get next info out
-			nextID = curBlockInfo['next']; //Block that comes after has key 'next'
-
-			//If the block is not a script (i.e. it's an event but doesn't have anything after), return empty dictionary
-			if((nextID == null) && (event_opcodes.includes(opcode))){
-				return [];
-			}
-			script.push(curBlockInfo); //Add the block itself to the script dictionary                
-			//Iterate: Set next to curBlock
-			curBlockID = nextID;
-		}
-		return script;
-	}
-};
-
-class GradeOneWaySyncL2 {
-
-	constructor() {
-		this.requirements = {};
-		this.extensions = {};
-	}
-
-	initReqs() { //initialize all metrics to false
+	init() { 
 		this.requirements = {
 			hasBackdrop: {bool: false, str: 'Project has a backdrop'},
 			fiveSprites: {bool: false, str: 'Five sprites created'},
-			startButton: {bool: false, str: 'One sprite sends a broadcast'},
-			startToSprite0: {bool: false, str: 'Message received by sprite 1 and sprite does something'},
-			startToSprite1: {bool: false, str: 'Message received by sprite 2 and sprite does something'},
-			startToSprite2: {bool: false, str: 'Message received by sprite 3 and sprite does something'},
-			startToSprite3: {bool: false, str: 'Message received by sprite 4 and sprite does something'}
+			oneToMany: {bool: false, str: 'One sprite sends a broadcast'},
+			startToSprite1: {bool: false, str: 'Message received by sprite 1 and sprite does something'},
+			startToSprite2: {bool: false, str: 'Message received by sprite 2 and sprite does something'},
+			startToSprite3: {bool: false, str: 'Message received by sprite 3 and sprite does something'},
+			startToSprite4: {bool: false, str: 'Message received by sprite 4 and sprite does something'}
 		};
-	}
-
-	initExts() {
 		this.extensions = {
 			animated: {bool: false, str: 'Added animation to sprites'},
-			sayBlock: {bool: false, str: 'Added say blocks to sprites'}
+			sayBlock: {bool: false, str: 'Added say blocks to sprites'},
+			anotherOneToMany: {bool: false, str: 'Used one-to-many synchronization a second time'}
 		};
-	}
-
-	checkSayBlock(sprites) {
-		for(var sprite of sprites) {
-			const sayBlocks = sb3.findBlockIDs(sprite.blocks, 'looks_say');
-			sayBlocks.concat(sb3.findBlockIDs(sprite.blocks, 'look_sayforsecs'));
-			if(sayBlocks.length > 0) {
-				this.extensions.sayBlock.bool = true;;
-				return;
-			}
-		}
-	}
-
-	checkAnimation(sprites) {
-		for(var sprite of sprites) {
-			var changeCostume = false;
-			var waitBlock = false;
-			const repeatBlocks = sb3.findBlockIDs(sprite.blocks, 'control_repeat');
-			if (repeatBlocks) {
-				for(var repeatBlock of repeatBlocks) {
-					const script = sb3.makeScript(sprite.blocks, repeatBlock);
-					for (var block of script) {
-						switch(block.opcode) {
-							case 'looks_switchcostumeto':
-							case 'looks_nextcostume': {
-								changeCostume = true;
-								break;
-							}
-							case 'control_wait': {
-								waitBlock = true;
-							}
-						}
-					}
-				}
-			}
-			if (changeCostume && waitBlock) {
-				this.extensions.animated.bool = true;
-				return;
-			}
-		}
-	}
-
-	checkOneToFive(sprites) {
-		// makes sure there are sprites to check for
-		if (sb3.no(sprites)) {
-			return;
-		}
-		// object to record what messages have been sent and received
-		var messages = {};
-
-		for(var sprite of sprites) {
-			// iterate through "when sprite clicked" events in search of message
-			const whenClickedBlocks = sb3.findBlockIDs(sprite.blocks, 'event_whenthisspriteclicked');
-			if(whenClickedBlocks) {
-				for(var whenClickedBlock of whenClickedBlocks) {
-					const script = sb3.makeScript(sprite.blocks, whenClickedBlock);
-					for(var block of script){
-						if(['event_broadcast', 'event_broadcastandwait'].includes(block.opcode)) {
-							this.requirements.startButton.bool = true;
-							const msg = block.inputs.BROADCAST_INPUT[1][1];
-							if(msg in messages)
-								messages[msg][0] = true;
-							else
-								messages[msg] = [true, []];
-						} 
-					}
-				}
-			}
-			// iterate through "when received" blocks in search of a message
-			const whenReceivedBlocks = sb3.findBlockIDs(sprite.blocks, 'event_whenbroadcastreceived');
-			if(whenReceivedBlocks) {
-				for(var whenReceivedBlock of whenReceivedBlocks) {
-					const script = sb3.makeScript(sprite.blocks, whenReceivedBlock);
-					const msg = script[0].fields.BROADCAST_OPTION[0]
-					if (msg in messages)
-						messages[msg][1].push(sprite.name);
-					else
-						messages[msg] = [false, [sprite.name]];
-				}
-			}
-		}
-		// complicated way of finding the most received message.
-		// this could be simplifed if it is extraneous who received the message
-		//   and what it was
-		return Object.keys(messages).reduce((prev, msg) => {
-			if(messages[msg][0] && (messages[msg][1].length > prev.receivedBy.length))
-				return {message: msg, receivedBy: messages[msg][1]};
-			else 
-				return prev;
-		}, {message: '', receivedBy: []});
 	}
 
 	grade(fileObj, user) { 
-		this.initReqs();
-		this.initExts();
-		var sprites = [];
-		const targets = fileObj.targets;
+		this.init();
+		let project = new Project(fileObj);
 		
-		// creates array of sprites, checks whether the backdrop has been set
-		for (var target of targets) {
-			if (target.isStage) {
-				this.requirements.hasBackdrop.bool = (target.costumes.length > 0)
-			} else {
-				sprites.push(target);
+		this.requirements.hasBackdrop.bool = project.targets[0].costumes.length > 1 || project.targets[0].costumes[0].name !== 'backdrop1';
+		let reports = project.sprites.map(sprite => this.gradeSprite(sprite));
+
+		let messages = {};
+
+		for (let report of reports) {
+			if (report.sent != []) {
+				for (let msg of report.sent) {
+					if (msg in messages) messages[msg].sent = true;
+					else messages[msg] = {sent: true, recipients: []};
+				}
+			}
+			if (report.received != []) {
+				for (let msg of report.received) {
+					if (msg in messages) messages[msg].recipients.push(report.name);
+					else messages[msg] = {sent: false, recipients: [report.name]};
+				}
 			}
 		}
-		// if more 5 or more sprites
-		this.requirements.fiveSprites.bool = (sprites.length >= 5);
-		// find the message with the most successful receives
-		var {message, receivedBy} = this.checkOneToFive(sprites);
-		
-		// for every successful message received, set requriement to true
-		receivedBy = receivedBy > 4 ? 4 : receivedBy;
-		for(var i in receivedBy) {
-			this.requirements['startToSprite' + i].bool = true;
+
+		let oneToFourCount = 0;
+
+		messages = Object.entries(messages).sort((a, b) => b[1].recipients.length - a[1].recipients.length);
+		if (messages.length > 0) {
+			for (let [msg, {sent, recipients}] of messages) {
+				if (sent) {
+					for (let sender of reports.filter(r => r.sent.includes(msg))) {
+						let count = (recipients.length - recipients.includes(sender));
+						for (let i = Math.min(count, 4); i > 0; i--) {
+							this.requirements[`startToSprite${i}`].bool = true;
+						}
+						oneToFourCount += count >= 4;
+					}
+				}
+			}	 
 		}
-		// check for animation and say block. Does not check if these blocks
-		//  are a result of the broadcast
-		this.checkAnimation(sprites);
-		this.checkSayBlock(sprites);
+
+		this.requirements.fiveSprites.bool = (reports.length >= 5);
+		this.extensions.anotherOneToMany.bool = (oneToFourCount > 1);
+	}
+	
+	gradeSprite(sprite) {
+		let reqs = {
+			name: sprite.name,
+			sent: [],
+			received: []
+		}
+		for (let script of sprite.scripts.filter(s => s.blocks[0].opcode.includes('event_when'))) {
+			
+			if (script.blocks[0].opcode === 'event_whenbroadcastreceived' && script.blocks.length > 1) {
+				reqs.received.push(script.blocks[0].fields.BROADCAST_OPTION[0]);
+			}
+			let animated = {costume: false, move: false, wait: false};
+			script.traverseBlocks((block, level) => {
+
+				if (['event_broadcast', 'event_broadcastandwait'].includes(block.opcode)) {
+					reqs.sent.push( block.inputs.BROADCAST_INPUT[1][1]);
+					this.requirements.oneToMany.bool = true;
+				}
+
+				if (['looks_say', 'looks_sayforsecs'].includes(block.opcode))
+					this.extensions.sayBlock.bool = true;
+
+				if (level > 1) {
+					if (['event_blooks_switchcostumeto', 'looks_nextcostume'].includes(block.opcode)) {
+						animated.costume = true;
+					} else if (block.opcode === 'control_wait') {
+						animated.wait = true;	
+					} else if (block.opcode.includes('motion_')) {
+						animated.motion = true;
+					}
+				}
+			}, ['control_forever', 'control_repeat', 'control_repeat_until']);
+
+			if (animated.wait && (animated.move || animated.costume)) {
+				this.extensions.animated.bool = true;
+			}	
+		}
+		return reqs;
 	}
 }
-
-module.exports = GradeOneWaySyncL2;
