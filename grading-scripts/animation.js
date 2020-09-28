e9e91774c5ada0098b1319b37233db09af6b0f91
Zack Crenshaw <zcrenshaw@cs.uchicago.edu>
2020-09-28 12:46:23 -0500

    patch grading website

--- grading-scripts/animation.js
+++ grading-scripts/animation.js
@@ -5,19 +5,19 @@ Scratch 3 updates: Elizabeth Crowdus, Zachary Crenshaw Spring 2019
 
 var sb3 = {
     //null checker
-    no: function(x) { 
+    no: function(x) {
         return (x == null || x == {} || x == undefined || !x || x == '' | x.length === 0);
     },
 
     //retrieve a given sprite's blocks from JSON
     //note: doesn't check whether or not blocks are properly attached
-    jsonToSpriteBlocks: function(json, spriteName) { 
+    jsonToSpriteBlocks: function(json, spriteName) {
         if (this.no(json)) return []; //make sure script exists
 
         var projInfo = json['targets'] //extract targets from JSON data
         var allBlocks={};
         var blocks={};
-        
+
         //find sprite
         for(i=0; i <projInfo.length; i++){
             if(projInfo[i]['name'] == spriteName){
@@ -26,13 +26,13 @@ var sb3 = {
         }
         return [];
     }, //done
-    
+
     //retrieve a given sprite's info (not just blocks) from JSON
-    jsonToSprite: function(json, spriteName) { 
+    jsonToSprite: function(json, spriteName) {
         if (this.no(json)) return []; //make sure script exists
 
         var projInfo = json['targets'] //extract targets from JSON data
-        
+
         //find sprite
         for(i=0; i <projInfo.length; i++){
             if(projInfo[i]['name'] == spriteName){
@@ -41,14 +41,14 @@ var sb3 = {
         }
         return [];
     }, //done
-    
+
     //counts the number of non-background sprites in a project
     countSprites: function(json){
         if (this.no(json)) return false; //make sure script exists
-        
+
         var numSprites = 0;
         var projInfo = json['targets'] //extract targets from JSON data
-        
+
         for(i=0; i <projInfo.length; i++){
             if(projInfo[i]['isStage'] == false){
                 numSprites ++;
@@ -56,14 +56,14 @@ var sb3 = {
         }
         return numSprites
     },
-    
+
     //looks through json to see if a sprite with a given name is present
     //returns true if sprite with given name found
-    findSprite: function(json, spriteName){ 
+    findSprite: function(json, spriteName){
         if (this.no(json)) return false; //make sure script exists
 
         var projInfo = json['targets'] //extract targets from JSON data
-        
+
         //find sprite
         for(i=0; i <projInfo.length; i++){
             if(projInfo[i]['name'] == spriteName){
@@ -72,28 +72,28 @@ var sb3 = {
         }
         return false;
     }, //done
-    
+
     //returns list of block ids given a set of blocks
     findBlockIDs: function(blocks, opcode){
         if(this.no(blocks) || blocks == {}) return [];
-        
+
         var blockids = [];
-        
-        for(block in blocks){ 
+
+        for(block in blocks){
             if(blocks[block]['opcode'] == opcode){
                 blockids.push(block);
             }
         }
         return blockids;
     },
-    
-    //given particular key, returns list of block ids of a certain kind of key press given a set of blocks 
+
+    //given particular key, returns list of block ids of a certain kind of key press given a set of blocks
     findKeyPressID: function(blocks, key){
         if(this.no(blocks) || blocks == {}) return [];
-        
+
         var blockids = [];
-        
-        for(block in blocks){ 
+
+        for(block in blocks){
             if(blocks[block]['opcode'] == 'event_whenkeypressed'){
                 if(blocks[block]['fields']['KEY_OPTION'][0] == key){
                     blockids.push(block);
@@ -102,10 +102,10 @@ var sb3 = {
         }
         return blockids;
     },
-    
+
     opcodeBlocks: function(script, myOpcode) { //retrieve blocks with a certain opcode from a script list of blocks
         if (this.no(script)) return [];
-        
+
         var miniscript = [];
 
         for(block in script){
@@ -114,29 +114,29 @@ var sb3 = {
             }
         }
         return miniscript;
-    }, 
-    
-    opcode: function(block) { //retrives opcode from a block object 
+    },
+
+    opcode: function(block) { //retrives opcode from a block object
         if (this.no(block)) return "";
         return block['opcode'];
-    }, 
-    
+    },
+
     countBlocks: function(blocks,opcode){ //counts number of blocks with a given opcode
         var total = 0;
-		for(id in blocks){ 
+		for(id in blocks){
             if([blocks][id]['opcode'] == opcode){
                 total = total + 1;
             }
         }
         return total;
     }, //done
-    
+
     //(recursive) helper function to extract blocks inside a given loop
     //works like makeScript except it only goes down the linked list (rather than down & up)
     loopExtract: function(blocks, blockID){
         if (this.no(blocks) || this.no(blockID)) return [];
         loop_opcodes = ['control_repeat', 'control_forever', 'control_if', 'control_if_else', 'control_repeat_until'];
-        
+
         var curBlockID = blockID;
         var script = [];
 
@@ -144,13 +144,13 @@ var sb3 = {
         curBlockID = blockID //Initialize with blockID of interest
         while(curBlockID != null){
             curBlockInfo = blocks[curBlockID]; //Pull out info about the block
-            script.push(curBlockInfo); //Add the block itself to the script dictionary                
+            script.push(curBlockInfo); //Add the block itself to the script dictionary
 
             //Get next info out
             nextID = curBlockInfo['next']; //Block that comes after has key 'next'
             //nextInfo = blocks[nextID]
             opcode = curBlockInfo['opcode'];
-            
+
             //extract nested children if loop block
             if(loop_opcodes.includes(opcode)){
                 var innerloop = curBlockInfo['inputs']['SUBSTACK'][1]
@@ -161,37 +161,37 @@ var sb3 = {
                     }
                 }
             }
-		
+
             //If the block is not a script (i.e. it's an event but doesn't have anything after), return empty dictionary
             if((nextID == null) && (event_opcodes.includes(opcode))){
                 return [];
             }
             //Iterate: Set next to curBlock
             curBlockID = nextID;
-        }     
-        return script;        
+        }
+        return script;
     },
-    
+
     //given list of blocks and a keyID of a block, return a script
     makeScript: function(blocks, blockID){
         if (this.no(blocks) || this.no(blockID)) return [];
         event_opcodes = ['event_whenflagclicked', 'event_whenthisspriteclicked','event_whenbroadcastreceived','event_whenkeypressed', 'event_whenbackdropswitchesto','event_whengreaterthan'];
         loop_opcodes = ['control_repeat', 'control_forever', 'control_if', 'control_if_else', 'control_repeat_until'];
-        
+
         var curBlockID = blockID;
         var script = [];
-    
+
         //find all blocks that come before
         while(curBlockID != null){
             var curBlockInfo = blocks[curBlockID]; //Pull out info about the block
-            script.push(curBlockInfo); //Add the block itself to the script dictionary 
-            
+            script.push(curBlockInfo); //Add the block itself to the script dictionary
+
             //Get parent info out
             var parentID = curBlockInfo['parent']; //Block that comes before has key 'parent'
             //parentInfo = blocks[parentID]
     		var opcode = curBlockInfo['opcode'];
 
-            
+
             //extract nested children if loop block
             if(loop_opcodes.includes(opcode)){
                 var innerloop = curBlockInfo['inputs']['SUBSTACK'][1]
@@ -202,7 +202,7 @@ var sb3 = {
                     }
                 }
             }
-            
+
             //If the block is not part of a script (i.e. it's the first block, but is not an event), return empty dictionary
             if ((parentID == null) && !(event_opcodes.includes(opcode))){
                 return [];
@@ -221,23 +221,23 @@ var sb3 = {
             nextID = curBlockInfo['next']; //Block that comes after has key 'next'
             //nextInfo = blocks[nextID]
             opcode = curBlockInfo['opcode'];
-            
+
             //extract nested children if loop block
             if(loop_opcodes.includes(opcode)){
                 var innerloop = curBlockInfo['inputs']['SUBSTACK'][1]
                 if(innerloop != undefined){
                     var nested_blocks = this.loopExtract(blocks, innerloop)
-                    for(b in nested_blocks){                            
+                    for(b in nested_blocks){
                         script.push(nested_blocks[b])
                     }
                 }
             }
-		
+
             //If the block is not a script (i.e. it's an event but doesn't have anything after), return empty dictionary
             if((nextID == null) && (event_opcodes.includes(opcode))){
                 return [];
             }
-            script.push(curBlockInfo); //Add the block itself to the script dictionary                
+            script.push(curBlockInfo); //Add the block itself to the script dictionary
             //Iterate: Set next to curBlock
             curBlockID = nextID;
         }
@@ -270,11 +270,11 @@ class GradeAnimation {
         this.extensions.extraPostFinishAnimation = {bool:false, str:'Bee changes costume during victory dance.'};;
         this.extensions.extraTurnBlock = {bool:false, str:'Bee uses turn block during victory dance.'};;
     }
-    
+
 
 
     grade(fileObj, user) {
-        
+
         this.initReqs();
         // for private method
         var that = this;
@@ -290,18 +290,18 @@ class GradeAnimation {
         var extraSnakeMoveSteps = -1;
         var winner = null;
         var victoryDance = {};
-        
+
         for(var i in fileObj['targets']){ //find bee
             var bee = fileObj['targets'][i]
             if(bee['name'] == 'Bee'){
                 break;
             }
         }
-        
+
         //make space key script
         var keyid = sb3.findKeyPressID(bee['blocks'], 'space');
         if(keyid != null){
-            
+
             this.requirements.handlesSpaceBar.bool = true;
             var spaceScript = sb3.makeScript(bee['blocks'], keyid);
             console.log(spaceScript);
@@ -309,12 +309,12 @@ class GradeAnimation {
                 //check for loop
                 if(validLoops.includes(spaceScript[i]['opcode'])){
                     this.requirements.spaceBarLoop.bool = true;
-                }  
+                }
                 //check for wait block
                 if(spaceScript[i]['opcode'] == 'control_wait'){
                     this.requirements.spaceBarWaitBlock.bool = true;
                 }
-                //check for movement 
+                //check for movement
                 if(validMoves.includes(spaceScript[i]['opcode'])){
                     this.requirements.spaceBarMovement.bool = true;
                 }
@@ -324,8 +324,8 @@ class GradeAnimation {
                 }
             }
         }
-        
-        
+
+
         //make down arrow script
         var arrowid = sb3.findKeyPressID(bee['blocks'], 'down arrow');
         if(arrowid != null){
@@ -335,7 +335,7 @@ class GradeAnimation {
                 //check for costume change
                 if(arrowScript[i]['opcode'] == 'looks_switchcostumeto' || arrowScript[i]['opcode'] == 'looks_nextcostume'){
                     this.requirements.downArrowCostumeChange.bool = true;
-                }  
+                }
                 //check for wait block
                 if(arrowScript[i]['opcode'] == 'control_wait'){
                     this.requirements.downArrowWaitBlock.bool = true;
@@ -344,10 +344,10 @@ class GradeAnimation {
         }
 
     }
-    
-    
-    
 
 
-}
+
+
+
+};
 module.exports = GradeAnimation;
