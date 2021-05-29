ab073bb46f78be0979739bca3def77df0ad3be7f
David GM <dagm@uchicago.edu>
2021-05-28 22:54:56 -0500

    Implemented grader for Gaming strand, now only YouthCulture is missing

--- grading-scripts-s3/one-way-sync-L1.js
+++ grading-scripts-s3/one-way-sync-L1.js
@@ -103,112 +103,141 @@
 
 		
 
-		reports = reports.reduce((acc, r) => {
+		let reports = rawReports.reduce((acc, r) => {
 			acc.push({
 				name: r.name,
 				plays: r.plays,
 				sent: 
-					r.sent.length === 0 ? null : r.sent.reduce((acc, msg) => {
-						acc[msg] = messages[msg].recipents;
-						return acc;
-					}, {}),
+				r.sent.length === 0 ? null : r.sent.reduce((acc, msg) => {
+					acc[msg] = messages[msg].recipents;
+					return acc;
+				}, {}),
 				received: r.received,
-				dances: r.dances
+				dances: r.dances,
+				movesTilPink: r.movesTilPink,
+
 			});
 			return acc;
 		}, []);
-		const sentCount = (sender) => 
-				Object.values(sender.sent).reduce((acc, b) => acc + b.length, 0) + !(sender.name.includes('ali') || sender.name.includes('avajo'));
+
+		let sentCount;
+		if (this.strand === "multicultural" ) {
+			sentCount = (sender) => 
+			Object.values(sender.sent).reduce((acc, b) => acc + b.length, 0) + !(sender.name.includes('ali') || sender.name.includes('avajo'));
+		} else if (this.strand === "gaming") {
+			sentCount = (sender) => 
+			Object.values(sender.sent).reduce((acc, b) => acc + b.length, 0) + !(sender.name.toLowerCase().includes('go') || sender.name.toLowerCase().includes('truck'));
+		}
 
 		let senders = reports.filter(r => r.sent).sort((a, b) => {
-				return sentCount(b) - sentCount(a);
+			return sentCount(b) - sentCount(a);
 		})
-		this.requirements.startButton.bool = reports.length >= 5;
+		if(this.requirements.startButton != null){
+			this.requirements.startButton.bool = reports.length >= 5;
+		} 
         // Checks the sprite with the most broadcasts, assuming that it must be the start button
-		if (senders.length >= 3) {
-			let startButton = senders[0];
-			
-			let totalRecipients = new Set([]);
-			for (let recipients of Object.values(startButton.sent)) {
-				let score = 0;
-				for (let name of recipients) {
-					let recipientReport = reports.find(r => r.name == name);
-					if (recipientReport.plays.onClick || recipientReport.plays.onBroadcast || recipientReport.dances) {
-						score++;
-						totalRecipients.add(name);
-					}
-				}
-				if (score >= 4) this.requirements.oneToMany.bool = true;
-			}
-			
-			if (totalRecipients.size > 0) {
-				for (let i = Math.min(totalRecipients.size, 4); i > 0; i--) 
-					this.requirements[`startToSprite${i}`].bool = true;
+        if (senders.length >= 3) {
+        	let startButton = senders[0];
+
+        	let totalRecipients = new Set([]);
+        	for (let recipients of Object.values(startButton.sent)) {
+        		let score = 0;
+        		for (let name of recipients) {
+        			let recipientReport = reports.find(r => r.name == name);
+        			if (recipientReport.plays.onClick || recipientReport.plays.onBroadcast || recipientReport.dances) {
+        				score++;
+        				totalRecipients.add(name);
+        			}
+        		}
+        		if (score >= 4) this.requirements.oneToMany.bool = true;
+        	}
+
+        	if (totalRecipients.size > 0) {
+        		for (let i = Math.min(totalRecipients.size, 4); i > 0; i--) 
+        			this.requirements[`broadcastToSprite${i}`].bool = true;
 				//remove this sprite
 				senders = senders.slice(1);
 			}
-        }
+		}
         // Check if at least two of remaining sprites do what is expected from Djembe and Flute sprites
         if (senders.length >= 2) {
 
-			let visitedFlute = false;
-			let probableDjembe = null;
+        	let visitedFlute = false;
+        	let probableDjembe = null;
 
-			const sumScore = (score) => !score? 0 : Object.values(score).reduce((a, b) => a + b, 0)
-			
-            for (let sender of senders) {    
-                for (let [msg, recipients] of Object.entries(sender.sent)) {
-					let score = {
-						uniqueMessage: msg.toLowerCase() != 'navajo',
-						senderPlays: (sender.plays.onClick || sender.plays.onBroadcast),
-						recipientDances: recipients.some(recipient => reports.find(r => r.name == recipient && r.dances))
-					};
-					
-                	if (score.recipientDances && score.senderPlays && !score.uniqueMessage && !visitedFlute) {
-						visitedFlute = true;
-					} else {
-						probableDjembe = sumScore(score) > sumScore(probableDjembe) ? score : probableDjembe;
-					}
-				}
-            }
-            [this.requirements.oneToOne.bool, this.requirements.djembePlays.bool, this.requirements.djembeToChild.bool] = Object.values(probableDjembe);
-        }		
-	}
-	gradeSprite(sprite) {
-		let reqs = {
-			name: sprite.name,
-			plays: {onClick: false, onBroadcast: false},
-			sent: [],
-			received: [],
-			dances: {costume: false, wait: false}
-		}
-		for (let script of sprite.scripts.filter(s => s.blocks[0].opcode.includes('event_when'))) {
-			if (script.blocks[0].opcode === 'event_whenthisspriteclicked')
-				script.traverseBlocks((block, level) => {
-					if (['sound_play', 'sound_playuntildone'].includes(block.opcode))
-						reqs.plays.onClick = true;
-					else if (['event_broadcast', 'event_broadcastandwait'].includes(block.opcode))
-						reqs.sent.push( block.inputs.BROADCAST_INPUT[1][1])
-				});
-			else if (script.blocks[0].opcode === 'event_whenbroadcastreceived') {
-				reqs.received.push(script.blocks[0].fields.BROADCAST_OPTION[0]);
-				script.traverseBlocks((block, level) => {
-					if (['sound_play', 'sound_playuntildone'].includes(block.opcode))
-						reqs.plays.onBroadcast = true;
-					else if (['event_blooks_switchcostumeto', 'looks_nextcostume'].includes(block.opcode))
-						reqs.dances.costume = true;
-					else if (block.opcode === 'control_wait') {
-						reqs.dances.wait = true;
-						if (block.inputs.DURATION[1][1] != .5) this.extensions.changeWait.bool = true;
-					}
-				});
-			} 
-			script.traverseBlocks((block, level) => {
-				if (['looks_say', 'looks_sayforsecs'].includes(block.opcode))
-					this.extensions.sayBlock.bool = true;
-			});
-		}
-		reqs.dances = reqs.dances.costume && reqs.dances.costume;
-		return reqs;
-	}
+        	const sumScore = (score) => !score? 0 : Object.values(score).reduce((a, b) => a + b, 0)
+
+        	for (let sender of senders) {    
+        		for (let [msg, recipients] of Object.entries(sender.sent)) {
+        			let score = {
+        				uniqueMessage: msg.toLowerCase() != 'navajo',
+        				senderPlays: (sender.plays.onClick || sender.plays.onBroadcast),
+        				recipientDances: recipients.some(recipient => reports.find(r => r.name == recipient && r.dances))
+        			};
+
+        			if (score.recipientDances && score.senderPlays && !score.uniqueMessage && !visitedFlute) {
+        				visitedFlute = true;
+        			} else {
+        				probableDjembe = sumScore(score) > sumScore(probableDjembe) ? score : probableDjembe;
+        			}
+        		}
+        	}
+        	[this.requirements.oneToOne.bool, this.requirements.sourceAction.bool, this.requirements.targetAction.bool] = Object.values(probableDjembe);
+        }
+
+        if (this.strand === "gaming") {
+        	this.requirements.targetAction.bool = rawReports.find(r => r.name === "Rally Car").movesTilPink;
+        }
+    }
+    gradeSprite(sprite) {
+    	let reqs = {
+    		name: sprite.name,
+    		plays: {onClick: false, onBroadcast: false},
+    		sent: [],
+    		received: [],
+    		dances: {costume: false, wait: false},
+    		movesTilPink : false,
+    		says: 0
+    	}
+    	let onClickActions;
+    	if (this.strand === "multicultural") {
+    		onClickActions = ['sound_play', 'sound_playuntildone'];
+    	} else if (this.strand === "gaming") {
+    		onClickActions = ['looks_say', 'looks_sayforsecs']
+    	}
+    	for (let script of sprite.scripts.filter(s => s.blocks[0].opcode.includes('event_when'))) {
+    		if (script.blocks[0].opcode === 'event_whenthisspriteclicked')
+    			script.traverseBlocks((block, level) => {
+    				if (onClickActions.includes(block.opcode))
+    					reqs.plays.onClick = true;
+    				else if (['event_broadcast', 'event_broadcastandwait'].includes(block.opcode))
+    					reqs.sent.push( block.inputs.BROADCAST_INPUT[1][1])
+    			});
+    		else if (script.blocks[0].opcode === 'event_whenbroadcastreceived') {
+    			reqs.received.push(script.blocks[0].fields.BROADCAST_OPTION[0]);
+    			let stopsOnPink = false
+    			script.traverseBlocks((block, level) => { 
+    				if (['sound_play', 'sound_playuntildone'].includes(block.opcode))
+    					reqs.plays.onBroadcast = true;
+    				else if (['event_blooks_switchcostumeto', 'looks_nextcostume'].includes(block.opcode))
+    					reqs.dances.costume = true;
+    				else if (block.opcode === 'control_wait') {
+    					reqs.dances.wait = true;
+    					if (block.inputs.DURATION[1][1] != .5) this.extensions.changeWait.bool = true;
+    				} else if(block.opcode === "motion_movesteps" && block.within != null 
+    					&& block.within.opcode === "control_repeat_until" && 
+    					block.within.conditionBlock.opcode === "sensing_touchingcolor" &&
+    					block.within.conditionBlock.inputs['COLOR'][1][1] === "#ed75ec"){
+    						reqs.movesTilPink = true
+    				}
+    			});
+    		} 
+    		script.traverseBlocks((block, level) => {
+    			if (['looks_say', 'looks_sayforsecs'].includes(block.opcode))
+    				reqs.says += 1;
+    		});
+    	}
+    	reqs.dances = reqs.dances.costume && reqs.dances.costume;
+    	return reqs;
+    }
 }
