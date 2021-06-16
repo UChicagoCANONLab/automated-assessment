fb792dc4b0d20a3e2089c3192e2bafba79a90077
Zack Crenshaw <zcrenshaw@cs.uchicago.edu>
2021-06-15 23:46:10 -0500

    pilot autograder

--- edx/autograder/autograder.py
+++ edx/autograder/autograder.py
@@ -1,10 +1,7 @@
 from xqueue_watcher.grader import Grader
-import time
-from path import Path
 import subprocess
-import os
 import html
-import traceback
+import JSON
 
 def format_errors(errors):
     esc = html.escape
@@ -16,162 +13,75 @@ def format_errors(errors):
         error_string = '<div>{0}</div>'.format(error_string)
     return error_string
 
-def demote(user_uid, user_gid):
-    def result():
-        os.setgid(user_gid)
-        os.setuid(user_uid)
-    return result
-
-def check_file(response):
-  errors = []
-  for i, line in enumerate(response.split("\n"), 1):
-    error = None
-    if line.find("import os") >= 0:
-      error = f"line {i}: You are no allowed to use the os package"
-    if line.find("import sys") >= 0:
-      error = f"line {i}: You are no allowed to use the sys package"
-    if line.find("import shutil") >= 0:
-      error = f"line {i}: You are no allowed to use the shutil package"
-    if line.find("import pickle") >= 0:
-      error = f"line {i}: You are no allowed to use the pickle package"
-    if line.find("import multiprocessing") >= 0:
-      error = f"line {i}: You are no allowed to use the multiprocessing package"
-    if line.find("import subprocess") >= 0:
-      error = f"line {i}: You are no allowed to use the subprocess package"
-    if line.find("import requests") >= 0:
-      error = f"line {i}: You are no allowed to use the requests package"
-    if line.find("import socket") >= 0:
-      error = f"line {i}: You are no allowed to use the socket package"
-    if line.find("exec(") >= 0:
-      error = f"line {i}: You are no allowed to use the exec function"
-    if line.find("open(") >= 0:
-      error = f"line {i}: You are no allowed to use the open function"
-    if line.find("read(") >= 0:
-      error = f"line {i}: You are no allowed to use the read function"
-    if line.find("write(") >= 0:
-      error = f"line {i}: You are no allowed to use the write function"
-    if error is not None:
-      errors.append(error)
-
-  if len(errors) == 0:
-    errors = None
-  return errors
-
 class ScratchGrader(Grader):
+    results_template = """
+       <div>
+          <p> {error} </p>
+          <div id="report">
+            {report_contents}
+            </div>
+      </div"""
+    def grade(self, grader_path, grader_config, student_response):
+        problem = grader_config['module']
+        while os.path.exists(current_file):
+          no_ending = current_file.split(".")
+          current_file = no_ending + "-" + str(index) + ".py"
+          index += 1
+        student_id = g
+        results = {"correct": False,
+                    "score": 0,
+                    "msg": ''}
+        exec_args = ["node", "/home/scratch_encore/automated_assessment/edx/autograder/xqueue_grader.js", problem, student_id ]
+        # 1001 is the groupid and userid for the sandbox account
+        proc = subprocess.Popen(exec_args, preexec_fn=demote(1001, 1001), stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+        try:
+          return_code = proc.wait(timeout=10)
+        except subprocess.TimeoutExpired as e:
+            results["msg"] = "Unable to grade project in 10 seconds. Check your internet connection."
+            return results
+        visible = "false"
+        error = ""
+        score = 0
+        in_report = False
+        in_grade_script = False
+        report_list = []
+        for line in proc.stdout:
+            if line == "start_grade_script":
+                in_grade_script = True
+            elif line == "end_grade_script":
+                in_grade_script = False
+            elif in_grade_script:
+                # ignore any line in the grade script
+                continue
+            else:
+                if line.startswith("Error"):
+                    error = line
+                elif line == "report_start":
+                    in_report = True
+                    error = ""
+                elif line == "report_end":
+                    in_report = False
+                elif in_report:
+                    report_list.append(line)
+                elif line.startswith("score:"):
+                    score = float(line.split(':')[1].strip())
+                else:
+                    continue
 
-  results_template = """
-<div class="test">
-<header>Run results</header>
-  <section>
-    <div class="shortform">
-    {status}
-    </div>
-    <div class="longform">
-      {errors}
-      {results}
-    </div>
-  </section>
-</div>"""
+        if len(report_list) > 0:
+            report_div = "<br>"
+            for item in report_list:
+                report_div += item
+                report_div += " <br> "
 
-  def _grade(self, grader_path, grader_config, student_response):
-    grader_file = Path(grader_path).dirname() / "grade.py"
-    problem = grader_config['grader']
-    current_file = "/tmp/" + str(int(time.time())) + ".py"
-    index = 0
-    while os.path.exists(current_file):
-      no_ending = current_file.split(".")
-      current_file = no_ending + "-" + str(index) + ".py"
-      index += 1
-    results = {"correct": False,
-                "score": 0,
-                "tests": [("Blank Space", "1")],
-                "errors": []}
-    errors = check_file(student_response)
-    if errors is not None:
-      results["errors"] += errors
-      return results
-    file_obj = open(current_file, "w+")
-    file_obj.write(student_response)
-    file_obj.close()
-    exec_args = ["/home/litteken/qcfa-grader/bin/python", str(grader_file), problem, current_file]
-    # 1001 is the groupid and userid for the sandbox account
-    proc = subprocess.Popen(exec_args, preexec_fn=demote(1001, 1001), stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
-    try:
-      return_code = proc.wait(timeout=10)
-    except subprocess.TimeoutExpired as e:
-      results["errors"].append("Unable to complete task in 10 seconds.")
-      os.remove(current_file)
-      return results
-    os.remove(current_file)
-    in_error = False
-    in_test = False
-    for line in proc.stderr:
-      if line.startswith("error:"):
-        results["errors"].append(line)
-      elif line == "---begin error---\n":
-        in_error = True
-        results["errors"].append("")
-        continue
-      elif line == "---end error---\n":
-        in_error = False
-        continue
-      elif line == "---begin test results---\n":
-        in_test = True
-        continue
-      elif line == "---end test results---\n":
-        in_test = False
-        continue
+            report_div += " <br>"
+        else:
+            report_div = ""
 
-      if in_error:
-        results["errors"][-1] += line
-      elif in_test:
-        line_split = line.strip("\n").split(":")
-        test = line_split[0]
-        score = line_split[1]
-        if test == "all":
-          score_split = score.split("/")
-          recieved = int(score_split[0])
-          possible = int(score_split[1])
-          assert recieved <= possible, "Score given is greater than possible score."
-          results["correct"] = recieved == possible
-          results["score"] = recieved / possible
-          continue
-        results["tests"].append((test, score))
-    if proc.returncode != 0 and len(results["errors"]) == 0:
-      raise Exception("Error in grade.py")
-    return results
+        results['score'] = score
+        results['msg'] = self.results_template.format(error=error,
+                                                report_contents=report_div)
+        return results
 
-  def grade(self, grader_path, grader_config, student_response):
-    try:
-      return self._grade(grader_path, grader_config, student_response)
-    except Exception as e:
-      with open('/home/litteken/grader-log.txt', 'a') as f:
-        f.write(">>>>> Error Begin\n")
-        f.write("Time: {}\n".format(time.ctime()))
-        f.write("----Traceback----\n")
-        f.write(str(e))
-        f.write(traceback.format_exc())
-        f.write("----grader_config----\n")
-        f.write(str(grader_config) + "\n")
-        f.write("----student_response----\n")
-        f.write(student_response + "\n")
-        f.write(">>>>> Error End\n")
-      return {"correct": False,
-              "score": 0,
-              "tests": [("Blank Space", "1")],
-              "errors": ["Please contact the course administrators to fix the problem, along with the time of this error: " + time.ctime()]}
-
-
-  def render_results(self, results):
-    if len(results['errors']) > 0:
-      errors = format_errors(results['errors'])
-    else:
-      errors = format_errors(["There were no errors running the code."])
-
-    status = 'Ran without errors'
-    if len(results['errors']) > 0:
-        status = 'ERROR running submitted code'
-
-    return self.results_template.format(status=status,
-                                        errors=errors,
-                                        results='')
+    def render_results(self, results):
+        return results
--- edx/autograder/xqueue_grader.js
+++ edx/autograder/xqueue_grader.js
@@ -1,33 +1,33 @@
 
 /// Scratch Encore graders
 var graders = {
-    scratchBasicsL1:        { name: 'M1 - Scratch Basics L2',       file: require('./grading-scripts-s3/scratch-basics-L1') },
-    scratchBasicsL2_create: { name: 'M1 - Scratch Basics L3',       file: require('./grading-scripts-s3/scratch-basics-L2') },
-    eventsL1:               { name: 'M2 - Events L1',               file: require('./grading-scripts-s3/events-L1-syn') },
-    eventsL2_create:        { name: 'M2 - Events L2',               file: require('./grading-scripts-s3/events-L2') },
-    animationL1:            { name: 'M3 - Animation L1',            file: require('./grading-scripts-s3/animation-L1') },
-    animationL2_create:     { name: 'M3 - Animation L2',            file: require('./grading-scripts-s3/animation-L2') },
-    condLoopsL1:            { name: 'M4 - Conditional Loops L1',    file: require('./grading-scripts-s3/cond-loops-L1-syn') },
-    condLoopsL2_create:     { name: 'M4 - Conditional Loops L2',    file: require('./grading-scripts-s3/cond-loops-L2') },
-    decompL1:               { name: 'M5 - Decomp. by Sequence L1',  file: require('./grading-scripts-s3/decomp-L1') },
-    decompL2_create:        { name: 'M5 - Decomp. by Sequence L2',  file: require('./grading-scripts-s3/decomp-L2') },
-    oneWaySyncL1:           { name: 'M6 - One-Way Sync L1',         file: require('./grading-scripts-s3/one-way-sync-L1') },
-    oneWaySyncL2_create:    { name: 'M6 - One-Way Sync L2',         file: require('./grading-scripts-s3/one-way-sync-L2') },
-    twoWaySyncL1:           { name: 'M7 - Two-Way Sync L1',         file: require('./grading-scripts-s3/two-way-sync-L1') },
-    complexConditionalsL1:  { name: 'M8 - Complex Conditionals L1', file: require('./grading-scripts-s3/complex-conditionals-L1') },
+    scratchBasicsL1:        { name: 'M1 - Scratch Basics L2',       file: require('../../grading-scripts-s3/scratch-basics-L1') },
+    scratchBasicsL2_create: { name: 'M1 - Scratch Basics L3',       file: require('../../grading-scripts-s3/scratch-basics-L2') },
+    eventsL1:               { name: 'M2 - Events L1',               file: require('../../grading-scripts-s3/events-L1-syn') },
+    eventsL2_create:        { name: 'M2 - Events L2',               file: require('../../grading-scripts-s3/events-L2') },
+    animationL1:            { name: 'M3 - Animation L1',            file: require('../../grading-scripts-s3/animation-L1') },
+    animationL2_create:     { name: 'M3 - Animation L2',            file: require('../../grading-scripts-s3/animation-L2') },
+    condLoopsL1:            { name: 'M4 - Conditional Loops L1',    file: require('../../grading-scripts-s3/cond-loops-L1-syn') },
+    condLoopsL2_create:     { name: 'M4 - Conditional Loops L2',    file: require('../../grading-scripts-s3/cond-loops-L2') },
+    decompL1:               { name: 'M5 - Decomp. by Sequence L1',  file: require('../../grading-scripts-s3/decomp-L1') },
+    decompL2_create:        { name: 'M5 - Decomp. by Sequence L2',  file: require('../../grading-scripts-s3/decomp-L2') },
+    oneWaySyncL1:           { name: 'M6 - One-Way Sync L1',         file: require('../../grading-scripts-s3/one-way-sync-L1') },
+    oneWaySyncL2_create:    { name: 'M6 - One-Way Sync L2',         file: require('../../grading-scripts-s3/one-way-sync-L2') },
+    twoWaySyncL1:           { name: 'M7 - Two-Way Sync L1',         file: require('../../grading-scripts-s3/two-way-sync-L1') },
+    complexConditionalsL1:  { name: 'M8 - Complex Conditionals L1', file: require('../../grading-scripts-s3/complex-conditionals-L1') },
 };
 
 /// Act 1 graders
 var actOneGraders = {
-    scavengerHunt: { name: 'M1 - Scavenger Hunt',    file: require('./act1-grading-scripts/scavengerHunt') },
-    onTheFarm:     { name: 'M2 - On the Farm',       file: require('./act1-grading-scripts/onTheFarm') },
-    namePoem:      { name: 'M3 - Name Poem',         file: require('./act1-grading-scripts/name-poem') },
-    ofrenda:       { name: 'M4 - Ofrenda',           file: require('./act1-grading-scripts/ofrenda') },
-    aboutMe:       { name: 'M5 - About Me',          file: require('./act1-grading-scripts/aboutMe') },
-    animalParade:  { name: 'M6 - Animal Parade',     file: require('./act1-grading-scripts/animal-parade') },
-    danceParty:    { name: 'M7 - Dance Party',       file: require('./act1-grading-scripts/dance-party') },
-    knockKnock:    { name: 'M8 - Knock Knock',       file: require('./act1-grading-scripts/knockKnock') },
-    finalProject:  { name: 'M9 - Interactive Story', file: require('./act1-grading-scripts/final-project') },
+    scavengerHunt: { name: 'M1 - Scavenger Hunt',    file: require('../../act1-grading-scripts/scavengerHunt') },
+    onTheFarm:     { name: 'M2 - On the Farm',       file: require('../../act1-grading-scripts/onTheFarm') },
+    namePoem:      { name: 'M3 - Name Poem',         file: require('../../act1-grading-scripts/name-poem') },
+    ofrenda:       { name: 'M4 - Ofrenda',           file: require('../../act1-grading-scripts/ofrenda') },
+    aboutMe:       { name: 'M5 - About Me',          file: require('../../act1-grading-scripts/aboutMe') },
+    animalParade:  { name: 'M6 - Animal Parade',     file: require('../../act1-grading-scripts/animal-parade') },
+    danceParty:    { name: 'M7 - Dance Party',       file: require('../../act1-grading-scripts/dance-party') },
+    knockKnock:    { name: 'M8 - Knock Knock',       file: require('../../act1-grading-scripts/knockKnock') },
+    finalProject:  { name: 'M9 - Interactive Story', file: require('../../act1-grading-scripts/final-project') },
 };
 
 
@@ -42,6 +42,8 @@ for (let graderKeyList of [graders, actOneGraders]) {
 //Get project from url
 function get(url) {
     return new Promise(function (resolve, reject) {
+        var XMLHttpRequest = require('xhr2');
+        //var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
         let request = new XMLHttpRequest();
         request.open('GET', url);
         request.onload = resolve;
@@ -52,52 +54,62 @@ function get(url) {
 
 
 
-async function gradeOneProject(projectID) {
-
-    console.log('Grading project ' + projectID);
+async function gradeOneProject(projectID, report_dict, gradeObj) {
     /// Getting the project page from Scratch so we can see the teacher-facing usernames
     get('https://chord.cs.uchicago.edu/scratch/projectinfo/' + projectID)
         .then(async function (result) {
             var projectInfo = JSON.parse(result.target.response);
             if (projectInfo.length === 0 || projectInfo.targets === undefined){
-                if (!project_count) {
-                    console.log('Project ' + projectID + ' could not be found. Did you enter a valid Scratch project URL?');
-                    IS_LOADING = false;
-                }
+                    console.log('Error: Project ' + projectID + ' could not be found. Did you enter a valid Scratch project URL?');
+                    report_dict['error'] = true;
+            }
+            else {
+              report_dict['error'] = false;
             }
              /// Getting the project file itself
             get('https://chord.cs.uchicago.edu/scratch/project/' + projectID)
             .then(async function (result) {
                 var projectJSON = JSON.parse(result.target.response);
                 if (projectJSON.targets === undefined) {
-                    console.log('Project ' + projectID + ' could not be found');
+                    console.log('Error: Project ' + projectID + ' could not be found');
+                    report_dict['error'] = true;
                     return;
                 }
+                report_dict['error'] = false;
                 try {
-                    analyze(projectJSON, projectInfo.author.username, projectID);
+                    analyze(projectJSON, projectInfo.author.username, projectID, report_dict, gradeObj);
                 }
                 catch (err) {
-                    console.log('Error grading project ' + projectID);
-                    /// console.log(err);
+                    //console.log('Error grading project ' + projectID);
+                    //report_dict['error'] = err;
+                    //console.log(err);
                 }
             });
         });
 }
 
-function analyze(fileObj, user, id) {
+function analyze(fileObj, user, id, report_dict, gradeObj) {
     try {
+        console.log("start_grade_script");
         gradeObj.grade(fileObj, id);
+        console.log("end_grade_script");
     }
     catch (err) {
+        logger.enableLogger();
         console.log('Error grading project ' + id);
         console.log(err);
+        report_dict['error'] = err;
     }
-    report(id, gradeObj.requirements, gradeObj.extensions, user);
+    report(id, gradeObj.requirements, gradeObj.extensions, user, report_dict);
+
 
 }
 
 function clearReport() {
   return {
+                  "projectID": null,
+                  "module": null,
+                  "error" : false,
                   "progressBar":
                     {
                       "greenbar": {width : "", innerHTML : ""},
@@ -108,7 +120,8 @@ function clearReport() {
                   "report":
                     {
 
-                    }
+                    },
+                  "score" : 0
 
                 };
 }
@@ -121,39 +134,47 @@ function checkbox(bool) {
 
 
 /* Adds results to reports_list and prints. */
-function report(projectID, requirements, extensions, projectAuthor) {
+function report(projectID, requirements, extensions, projectAuthor, report_dict) {
     let ret_list = [];
-    let project_complete = true;
+    let total_reqs_count = 0;
     let passed_reqs_count = 0;
+    let complete_exts = 0;
+    let total_exts = 0;
 
     /* Makes a string list of grading results. */
     ret_list.push('Project ID: <a href="https://scratch.mit.edu/projects/' + projectID + '">' + projectID + '</a>');
     ret_list.push('Creator: <a href="https://scratch.mit.edu/users/' + projectAuthor + '">' + projectAuthor + '</a>');
-    ret_list.push(projectMode ? '\nTasks' : 'Requirements:');
+    ret_list.push('\nTasks');
     for (let x in requirements) {
-        if (!requirements[x].bool) project_complete = false;
-        else passed_reqs_count++;
+        total_reqs_count++;
+        if (requirements[x].bool) passed_reqs_count++;
         ret_list.push(checkbox(requirements[x].bool) + ' - ' + requirements[x].str);
     }
     if (extensions) {
-        ret_list.push(projectMode ? '\nIf you are done early:' : 'Extensions:')
+        ret_list.push('\nIf you are done early:');
+        total_exts++;
         for (let x in extensions) {
             if (extensions[x].bool) complete_exts++;
             ret_list.push(checkbox(extensions[x].bool) + ' - ' + extensions[x].str);
         }
     }
-    ret_list.push('');
-    return [ret_list, project_complete, passed_reqs_count];
+    console.log("report_start");
+    for (let x in ret_list){
+      console.log(ret_list[x]);
+    }
+    console.log("report_end");
+    report_dict = showProgressBar(report_dict, passed_reqs_count, total_reqs_count);
 
 }
 
 /* Update progress bar segment to new proportion. */
-function setProgress(bar, items, total_items, color) {
+function setProgress(name, bar, items, total_items, color) {
+    var projectMode = true;
     let width_percent = ((items / total_items) * 100);
     bar['width'] = width_percent + '%';
     if (items && color === 0) {
         if (items !== total_items) bar['innerHTML'] = items;
-        if (width_percent === 100) bar['innerHTML'] += projectMode ? 'All tasks done!' : ' done';
+        if (width_percent === 100) bar['innerHTML'] += 'All tasks done!';
         else if (width_percent >= 15) bar['innerHTML'] += projectMode ?
             items === 1 ?
                 ' task done' : ' tasks done'
@@ -170,32 +191,42 @@ function setProgress(bar, items, total_items, color) {
                 ' task not done' : ' tasks not done'
             : ' need time or help';
     }
+    console.log(name + ":"  + bar['width'] + ":" + bar['innerHTML']);
 }
 
 
-function showProgressBar(report) {
+function showProgressBar(report_dict, total_reqs, complete_reqs) {
     let green = 0;
     let yellow = 0;
     let red = 0;
     let count = 0;
-    console.log('pm');
     green = complete_reqs;
     yellow = 0;
     red = total_reqs - complete_reqs;
-    console.log(red);
     count = total_reqs;
-    console.log(count);
-    setProgress(report['myProgress']['greenbar'], yellow, count, 1);
-    setProgress(report['myProgress']['greenbar'], yellow, count, 1);
-    setProgress(report['myProgress']['greenbar'], red, count, 2);
+    setProgress('green', report_dict['progressBar']['greenbar'], green, count, 0);
+    setProgress('yellow', report_dict['progressBar']['yellowbar'], yellow, count, 1);
+    setProgress('red', report_dict['progressBar']['redbar'], red, count, 2);
+    console.log('score: ' + parseFloat(green / count));
+
 }
 
 //////SCRIPT
 
-var myArgs = process.argv.slice(2);
-this_module = myArgs[0]
-report_dict = clearReport()
-graderObj = new allGraders[this_module].file
-console.log(report_dict)
+function run(){
+  var myArgs = process.argv.slice(2);
+  var this_module = myArgs[0];
+  var projectID = myArgs[1];
+  var report_dict = clearReport();
+  var gradeObj = new allGraders[this_module].file;
+  report_dict['module'] = this_module;
+  report_dict['projectID'] = projectID;
+  try {
+    gradeOneProject(projectID, report_dict, gradeObj);
+  }
+  catch(err){
+    console.log(err);
+  }
+}
 
-return 2
+run();
